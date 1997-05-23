726c96155380e03aff6e60e9656adaead8304952
curt <curt>
1997-05-23 15:40:04 +0000

    Added GNU copyright headers.

--- Aircraft/aircraft.c
+++ Aircraft/aircraft.c
@@ -3,6 +3,22 @@
  *
  * Written by Curtis Olson, started May 1997.
  *
+ * Copyright (C) 1997  Curtis L. Olson  - curt@infoplane.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
  * $Id$
  * (Log is kept at end of this file)
  **************************************************************************/
@@ -29,7 +45,10 @@ void aircraft_debug(int type) {
 
 
 /* $Log$
-/* Revision 1.1  1997/05/16 15:58:24  curt
-/* Initial revision.
+/* Revision 1.2  1997/05/23 15:40:29  curt
+/* Added GNU copyright headers.
 /*
+ * Revision 1.1  1997/05/16 15:58:24  curt
+ * Initial revision.
+ *
  */
