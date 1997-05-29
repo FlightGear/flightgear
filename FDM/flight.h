c994ed7feb22309842150fc0f5c2c3e90f003092
curt <curt>
1997-05-29 02:32:25 +0000

    Starting to build generic flight model interface.

--- FDM/flight.h
+++ FDM/flight.h
@@ -28,11 +28,20 @@
 #define FLIGHT_H
 
 
-#include "slew.h"
+#include "slew/slew.h"
+#include "LaRCsim/ls_interface.h"
+
+
+/* Define the various supported flight models (not all implemented) */
+#define FG_SLEW      0
+#define FG_LARCSIM   1
+#define FG_ACM       2
+#define FG_HELO      3
+#define FG_BALLOON   4
+#define FG_PARACHUTE 5
 
 
 /* Define a structure containing the shared flight model parameters */
-
 struct flight_params {
     double pos_x, pos_y, pos_z;   /* temporary position variables */
     double vel_x, vel_y, vel_z;   /* temporary velocity variables */
@@ -46,13 +55,25 @@ struct flight_params {
 };
 
 
+/* General interface to the flight model routines */
+
+/* Initialize the flight model parameters */
+int fgFlightModelInit(int model);
+
+/* Run an iteration of the flight model */
+int fgFlightModelUpdate(int model);
+
+
 #endif FLIGHT_H
 
 
 /* $Log$
-/* Revision 1.2  1997/05/23 15:40:37  curt
-/* Added GNU copyright headers.
+/* Revision 1.3  1997/05/29 02:32:25  curt
+/* Starting to build generic flight model interface.
 /*
+ * Revision 1.2  1997/05/23 15:40:37  curt
+ * Added GNU copyright headers.
+ *
  * Revision 1.1  1997/05/16 16:04:45  curt
  * Initial revision.
  *
