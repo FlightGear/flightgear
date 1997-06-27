2f5bc89a7a1654cd313f075ede953396096f239b
curt <curt>
1997-06-27 02:26:30 +0000

    Initial revision.

---
+++ Scenery/scenery.c
@@ -0,0 +1,57 @@
+/**************************************************************************
+ * scenery.c -- data structures and routines for managing scenery.
+ *
+ * Written by Curtis Olson, started May 1997.
+ *
+ * Copyright (C) 1997  Curtis L. Olson  - curt@infoplane.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ * (Log is kept at end of this file)
+ **************************************************************************/
+
+
+#include "scenery.h"
+#include "ParseVrml/parsevrml.h"
+
+
+/* Initialize the Scenery Management system */
+void fgSceneryInit() {
+    /* nothing to do here yet */
+}
+
+
+/* Tell the scenery manager where we are so it can load the proper data, and
+ * build the proper structures. */
+void fgSceneryUpdate(double lon, double lat, double elev) {
+    /* a hardcoded hack follows */
+
+    /* this routine should parse the file, and make calls back to the
+     * scenery management system to build the appropriate structures */
+    fgParseVRML("test.wrl");
+}
+
+
+/* Render out the current scene */
+void fgSceneryRender() {
+}
+
+
+/* $Log$
+/* Revision 1.1  1997/06/27 02:26:30  curt
+/* Initial revision.
+/*
+ */
