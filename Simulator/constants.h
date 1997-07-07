c350e0bdcae49532c786e8832257c946710b4848
curt <curt>
1997-07-07 21:02:36 +0000

    Initial revision.

---
+++ Simulator/constants.h
@@ -0,0 +1,55 @@
+/**************************************************************************
+ * constants.h -- various constant definitions
+ *
+ * Written by Curtis Olson, started July 1997.
+ *
+ * Copyright (C) 1997  Curtis L. Olson  - curt@infoplane.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ * (Log is kept at end of this file)
+ **************************************************************************/
+
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+
+/* Radius of Earth in meters at the equator.  Another source had
+ * 6378165 but this is probably close enough */
+#define EARTH_RAD 6378.155
+
+
+/* Degrees to Radians */
+#define DEG_TO_RAD       0.017453292
+
+/* Radians to Degrees */
+#define RAD_TO_DEG       57.29577951
+
+/* Arc seconds to radians */
+#define ARCSEC_TO_RAD 4.84813681109535993589e-06
+
+/* Feet to Meters */
+#define FEET_TO_METER    0.3048
+
+
+#endif CONSTANTS_H
+
+
+/* $Log$
+/* Revision 1.1  1997/07/07 21:02:36  curt
+/* Initial revision.
+/* */
