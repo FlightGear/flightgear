b6789508b270ad264c685e7050eab5f61b44813f
curt <curt>
1997-08-23 11:37:12 +0000

    Initial revision.

---
+++ Simulator/general.h
@@ -0,0 +1,49 @@
+/**************************************************************************
+ * general.h -- a general house keeping data structure definition for 
+ *              various info that might need to be accessible from all 
+ *              parts of the sim.
+ *
+ * Written by Curtis Olson, started July 1997.
+ *
+ * Copyright (C) 1997  Curtis L. Olson  - curt@infoplane.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ * (Log is kept at end of this file)
+ **************************************************************************/
+
+
+#ifndef GENERAL_H
+#define GENERAL_H
+
+
+/* the general house keeping structure definition */
+struct general_params {
+    /* The flight gear "root" directory */
+    char *root_dir;
+};
+
+/* general contains all the general house keeping parameters. */
+extern struct general_params general;
+
+#endif /* GENERAL_H */
+
+
+/* $Log$
+/* Revision 1.1  1997/08/23 11:37:12  curt
+/* Initial revision.
+/*
+ */
