db9fbe603f8d130e5f83f65287e579483cb2d267
curt <curt>
1998-01-24 13:03:21 +0000

    Initial revision.  Taken from Mark Kilgard's GLUT examples.

---
+++ Scenery/texload.h
@@ -0,0 +1,19 @@
+
+/* Copyright (c) Mark J. Kilgard, 1997.  */
+
+/* This program is freely distributable without licensing fees 
+   and is provided without guarantee or warrantee expressed or 
+   implied. This program is -not- in the public domain. */
+
+
+#ifndef _TEXLOAD_H
+#define _TEXLOAD_H
+
+
+#include <GL/glut.h>
+
+extern GLubyte *read_alpha_texture(char *name, int *width, int *height);
+extern GLubyte * read_rgb_texture(char *name, int *width, int *height);
+
+
+#endif /* _TEXLOAD_H */
