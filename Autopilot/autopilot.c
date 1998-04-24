090ebffc8465e1defdaefb6fca36f2341448e4aa
curt <curt>
1998-04-24 00:45:17 +0000

    Wrapped "#include <config.h>" in "#ifdef HAVE_CONFIG_H"

--- Autopilot/autopilot.c
+++ Autopilot/autopilot.c
@@ -24,7 +24,9 @@
  **************************************************************************/
 
 
-#include <config.h>
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
 
 #include <assert.h>
 #include <stdlib.h>
