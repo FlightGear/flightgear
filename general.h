9ecbe3b8aee4f34f7ad0feb9690079145eac7a66
curt <curt>
1998-05-07 23:03:17 +0000

    Lowered size of frame rate history buffer.

--- general.h
+++ general.h
@@ -30,7 +30,7 @@
 #define _GENERAL_H
 
 
-#define FG_FRAME_RATE_HISTORY 20
+#define FG_FRAME_RATE_HISTORY 10
 
 
 /* the general house keeping structure definition */
@@ -50,9 +50,12 @@ extern fgGENERAL general;
 
 
 /* $Log$
-/* Revision 1.4  1998/05/06 03:14:30  curt
-/* Added a shared frame rate counter.
+/* Revision 1.5  1998/05/07 23:03:17  curt
+/* Lowered size of frame rate history buffer.
 /*
+ * Revision 1.4  1998/05/06 03:14:30  curt
+ * Added a shared frame rate counter.
+ *
  * Revision 1.3  1998/03/14 00:27:58  curt
  * Promoted fgGENERAL to a "type" of struct.
  *
