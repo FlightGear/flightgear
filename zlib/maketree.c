15790ef4b43eb5f42f2724dc8488020bb8c37ae2
curt <curt>
1998-05-29 20:30:11 +0000

    Borland C++ fixes from the zlib author.  A new version is due out soon.

--- zlib/maketree.c
+++ zlib/maketree.c
@@ -68,9 +68,9 @@ void maketree(uInt b, inflate_huft *t)
       fprintf(stderr, "maketree: cannot initialize sub-tables!\n");
       exit(1);
     }
-    if (i % 5 == 0)
+    if (i % 4 == 0)
       printf("\n   ");
-    printf(" {{%u,%u},%u}", t[i].exop, t[i].bits, t[i].base);
+    printf(" {{{%u,%u}},%u}", t[i].exop, t[i].bits, t[i].base);
     if (++i == (1<<b))
       break;
     putchar(',');
