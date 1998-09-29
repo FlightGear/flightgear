e929f387a5d8f05689412ef1deef5a29e79e3de7
curt <curt>
1998-09-29 14:57:00 +0000

    c++-ified some comments.

--- Controls/controls.h
+++ Controls/controls.h
@@ -1,27 +1,25 @@
-/**************************************************************************
- * controls.h -- defines a standard interface to all flight sim controls
- *
- * Written by Curtis Olson, started May 1997.
- *
- * Copyright (C) 1997  Curtis L. Olson  - curt@infoplane.com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- * $Id$
- * (Log is kept at end of this file)
- **************************************************************************/
+// controls.h -- defines a standard interface to all flight sim controls
+//
+// Written by Curtis Olson, started May 1997.
+//
+// Copyright (C) 1997  Curtis L. Olson  - curt@infoplane.com
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// $Id$
+// (Log is kept at end of this file)
 
 
 #ifndef _CONTROLS_H
@@ -94,58 +92,60 @@ double fgBrakeGet( void );
 #endif /* _CONTROLS_H */
 
 
-/* $Log$
-/* Revision 1.16  1998/09/29 02:01:32  curt
-/* Added a brake.
-/*
- * Revision 1.15  1998/04/25 22:06:27  curt
- * Edited cvs log messages in source files ... bad bad bad!
- *
- * Revision 1.14  1998/04/22 13:26:19  curt
- * C++ - ifing the code a bit.
- *
- * Revision 1.13  1998/04/21 17:02:35  curt
- * Prepairing for C++ integration.
- *
- * Revision 1.12  1998/02/09 22:56:48  curt
- * Removed "depend" files from cvs control.  Other minor make tweaks.
- *
- * Revision 1.11  1998/02/07 15:29:36  curt
- * Incorporated HUD changes and struct/typedef changes from Charlie Hotchkiss
- * <chotchkiss@namg.us.anritsu.com>
- *
- * Revision 1.10  1998/01/27 00:47:52  curt
- * Incorporated Paul Bleisch's <pbleisch@acm.org> new debug message
- * system and commandline/config file processing code.
- *
- * Revision 1.9  1998/01/22 02:59:31  curt
- * Changed #ifdef FILE_H to #ifdef _FILE_H
- *
- * Revision 1.8  1998/01/19 18:40:22  curt
- * Tons of little changes to clean up the code and to remove fatal errors
- * when building with the c++ compiler.
- *
- * Revision 1.7  1997/12/15 23:54:36  curt
- * Add xgl wrappers for debugging.
- * Generate terrain normals on the fly.
- *
- * Revision 1.6  1997/12/10 22:37:41  curt
- * Prepended "fg" on the name of all global structures that didn't have it yet.
- * i.e. "struct WEATHER {}" became "struct fgWEATHER {}"
- *
- * Revision 1.5  1997/08/27 03:30:02  curt
- * Changed naming scheme of basic shared structures.
- *
- * Revision 1.4  1997/07/23 21:52:18  curt
- * Put comments around the text after an #endif for increased portability.
- *
- * Revision 1.3  1997/05/31 19:16:27  curt
- * Elevator trim added.
- *
- * Revision 1.2  1997/05/23 15:40:33  curt
- * Added GNU copyright headers.
- *
- * Revision 1.1  1997/05/16 15:59:48  curt
- * Initial revision.
- *
- */
+// $Log$
+// Revision 1.17  1998/09/29 14:57:00  curt
+// c++-ified some comments.
+//
+// Revision 1.16  1998/09/29 02:01:32  curt
+// Added a brake.
+//
+// Revision 1.15  1998/04/25 22:06:27  curt
+// Edited cvs log messages in source files ... bad bad bad!
+//
+// Revision 1.14  1998/04/22 13:26:19  curt
+// C++ - ifing the code a bit.
+//
+// Revision 1.13  1998/04/21 17:02:35  curt
+// Prepairing for C++ integration.
+//
+// Revision 1.12  1998/02/09 22:56:48  curt
+// Removed "depend" files from cvs control.  Other minor make tweaks.
+//
+// Revision 1.11  1998/02/07 15:29:36  curt
+// Incorporated HUD changes and struct/typedef changes from Charlie Hotchkiss
+// <chotchkiss@namg.us.anritsu.com>
+//
+// Revision 1.10  1998/01/27 00:47:52  curt
+// Incorporated Paul Bleisch's <pbleisch@acm.org> new debug message
+// system and commandline/config file processing code.
+//
+// Revision 1.9  1998/01/22 02:59:31  curt
+// Changed #ifdef FILE_H to #ifdef _FILE_H
+//
+// Revision 1.8  1998/01/19 18:40:22  curt
+// Tons of little changes to clean up the code and to remove fatal errors
+// when building with the c++ compiler.
+//
+// Revision 1.7  1997/12/15 23:54:36  curt
+// Add xgl wrappers for debugging.
+// Generate terrain normals on the fly.
+//
+// Revision 1.6  1997/12/10 22:37:41  curt
+// Prepended "fg" on the name of all global structures that didn't have it yet.
+// i.e. "struct WEATHER {}" became "struct fgWEATHER {}"
+//
+// Revision 1.5  1997/08/27 03:30:02  curt
+// Changed naming scheme of basic shared structures.
+//
+// Revision 1.4  1997/07/23 21:52:18  curt
+// Put comments around the text after an #endif for increased portability.
+//
+// Revision 1.3  1997/05/31 19:16:27  curt
+// Elevator trim added.
+//
+// Revision 1.2  1997/05/23 15:40:33  curt
+// Added GNU copyright headers.
+//
+// Revision 1.1  1997/05/16 15:59:48  curt
+// Initial revision.
+//
