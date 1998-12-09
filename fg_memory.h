915b2d9d882aff63781e6737275c401551c1b2be
curt <curt>
1998-12-09 18:47:39 +0000

    Use C++ style comments.

--- fg_memory.h
+++ fg_memory.h
@@ -1,23 +1,22 @@
-/**************************************************************************
- * fg_memory.h -- memcpy/bcopy portability declarations
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- * $Id$
- * (Log is kept at end of this file)
- **************************************************************************/
+// fg_memory.h -- memcpy/bcopy portability declarations
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// $Id$
+// (Log is kept at end of this file)
+
 
 #ifndef _FG_MEMORY_H
 #define _FG_MEMORY_H
@@ -56,10 +55,13 @@
 
 #endif
 
-#endif /* _FG_MEMORY_H */
+#endif // _FG_MEMORY_H
 
-/* $Log$
-/* Revision 1.1  1998/12/07 21:07:25  curt
-/* Memory related portability improvements.
-/*
- */
+
+// $Log$
+// Revision 1.2  1998/12/09 18:47:39  curt
+// Use C++ style comments.
+//
+// Revision 1.1  1998/12/07 21:07:25  curt
+// Memory related portability improvements.
+//
