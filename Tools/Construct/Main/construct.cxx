65a220fb257f88f8e9b47e46e035d90fff82bd0c
curt <curt>
1999-05-03 11:29:36 +0000

    Added construct.cxx

---
+++ Tools/Construct/Main/construct.cxx
@@ -0,0 +1,33 @@
+// construct.cxx -- Class to manage the primary data used in the
+//                  construction process
+//
+// Written by Curtis Olson, started May 1999.
+//
+// Copyright (C) 1999  Curtis L. Olson  - curt@flightgear.org
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// $Id$
+
+
+#include "construct.hxx"
+
+
+// Constructor
+FGConstruct::FGConstruct() { }
+
+
+// Destructor
+FGConstruct::~FGConstruct() { }
