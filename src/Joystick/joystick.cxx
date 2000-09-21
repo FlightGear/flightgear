dcf9dd9a1bb7dba79162cac5d536d6cf4a01a24b
curt <curt>
2000-09-21 20:08:51 +0000

    Fixed another array overrun type bug.

--- src/Joystick/joystick.cxx
+++ src/Joystick/joystick.cxx
@@ -47,7 +47,7 @@ static const int MAX_JOYSTICKS = 2;
 static const int MAX_JOYSTICKS = 10;
 #endif
 static const int MAX_AXES = _JS_MAX_AXES;
-static const int MAX_BUTTONS = 10;
+static const int MAX_BUTTONS = 32;
 
 
 /**
@@ -63,7 +63,12 @@ static const char * axisNames[] = {
 };
 static const char * buttonNames[] = {
     "button0", "button1", "button2", "button3", "button4",
-    "button5", "button6", "button7", "button8", "button9"
+    "button5", "button6", "button7", "button8", "button9",
+    "button10", "button11", "button12", "button13", "button14",
+    "button15", "button16", "button17", "button18", "button19",
+    "button20", "button21", "button22", "button23", "button24",
+    "button25", "button26", "button27", "button28", "button29",
+    "button30", "button31"
 };
 
 
