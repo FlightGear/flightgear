145c137433219012ac37adfde6500a5a2121c6e6
curt <curt>
2001-06-01 17:13:03 +0000

    Detect Irix/CC compiler and substitute AR="CC -ar".

--- irix-hack.sh
+++ irix-hack.sh
@@ -2,10 +2,7 @@
 
 for n in `find . -name Makefile`; do \
    echo "Fixing file $n"; \
-   \
-   mv -f $n $n.ar-new; \
-   sed 's/^AR = ar/AR = CC -ar/g' $n.ar-new > $n;
-   \
    mv -f $n $n.ar-new; \
    sed 's/$(AR) cru /$(AR) -o /g' $n.ar-new > $n; \
+   rm -f $n.ar-new; \
 done
