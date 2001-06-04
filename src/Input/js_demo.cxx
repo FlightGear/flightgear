9124dd945d9142b667839761f57075d07e4d1585
curt <curt>
2001-06-04 21:26:53 +0000

    Files moved from ../Joystick to here.

---
+++ src/Input/js_demo.cxx
@@ -0,0 +1,110 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#ifdef HAVE_WINDOWS_H
+#  include <windows.h>                     
+#endif
+
+#include <string.h>		// plib/js.h should really include this !!!!!!
+#include <plib/js.h>
+
+#define Z 8
+
+int main ( int, char ** )
+{
+  jsJoystick *js[Z] ;
+  float      *ax[Z] ;
+  int i, j, t, useful[Z];
+
+  for ( i = 0; i < Z; i++ )
+      js[i] = new jsJoystick ( i ) ;
+
+  printf ( "Joystick test program.\n" ) ;
+  printf ( "~~~~~~~~~~~~~~~~~~~~~~\n" ) ;
+
+  t = 0;
+  for ( i = 0; i < Z; i++ )
+  { useful[i] = ! ( js[i]->notWorking () );
+    if ( useful[i] )
+         t++;
+    else printf ( "Joystick %i not detected\n", i ) ;
+  }
+  if ( t == 0 ) exit ( 1 ) ;
+
+  for ( i = 0; i < Z; i++ )
+    if ( useful[i] )
+       ax[i] = new float [ js[i]->getNumAxes () ] ;
+
+  for ( i = 0 ; i < Z ; i++ )
+    if ( useful[i] )
+       printf ( "+--------------------JS.%d----------------------", i ) ;
+
+  printf ( "+\n" ) ;
+
+  for ( i = 0 ; i < Z ; i++ )
+   if ( useful[i] )
+   {
+    if ( js[i]->notWorking () )
+      printf ( "|           ~~~ Not Detected ~~~             " ) ;
+    else
+    {
+      printf ( "| Btns " ) ;
+
+      for ( j = 0 ; j < js[i]->getNumAxes () ; j++ )
+        printf ( "Ax:%1d ", j ) ;
+
+      for ( ; j < 8 ; j++ )
+        printf ( "     " ) ;
+    }
+   }
+
+  printf ( "|\n" ) ;
+
+  for ( i = 0 ; i < Z ; i++ )
+    if ( useful[i] )
+      printf ( "+----------------------------------------------" ) ;
+
+  printf ( "+\n" ) ;
+
+  while (1)
+  {
+    for ( i = 0 ; i < Z ; i++ )
+    if ( useful[i] )
+     {
+      if ( js[i]->notWorking () )
+        printf ( "|  .   .   .   .   .   .   .   .   .   .   . " ) ;
+      else
+      {
+        int b ;
+
+        js[i]->read ( &b, ax[i] ) ;
+
+        printf ( "| %04x ", b ) ;
+
+	for ( j = 0 ; j < js[i]->getNumAxes () ; j++ )
+	  printf ( "%+.1f ", ax[i][j] ) ;
+
+	for ( ; j < 8 ; j++ )
+	  printf ( "  .  " ) ;
+      }
+     }
+
+    printf ( "|\r" ) ;
+    fflush ( stdout ) ;
+
+    /* give other processes a chance */
+
+#ifdef WIN32
+    Sleep ( 1 ) ;
+#elif defined(sgi)
+    sginap ( 1 ) ;
+#else
+    usleep ( 1000 ) ;
+#endif
+  }
+
+  return 0 ;
+}
+
+
---
+++ src/Input/jssuper.h
@@ -0,0 +1,65 @@
+// jssuper.h -- manage access to multiple joysticks
+//
+// Written by Tony Peden, started May 2001
+//
+// Copyright (C) 2001  Tony Peden (apeden@earthlink.net)
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+#ifndef _JSSUPER_H
+#define _JSSUPER_H
+
+
+#ifdef HAVE_WINDOWS_H
+#  include <windows.h>                     
+#endif
+
+#include <string.h>		// plib/js.h should really include this !!!!!!
+#include <plib/js.h>
+
+#define MAX_JOYSTICKS 8
+
+class jsSuper {
+  private:
+    int activeJoysticks;
+    int active[MAX_JOYSTICKS];
+    int currentJoystick;
+    int first, last;
+    jsJoystick* js[MAX_JOYSTICKS];
+    
+  public:  
+    jsSuper(void);
+    ~jsSuper(void);
+    
+    inline int getNumJoysticks(void) { return activeJoysticks; }
+    
+    inline int atFirst(void) { return currentJoystick == first; }
+    inline int atLast(void) { return currentJoystick == last; }
+    
+    inline void firstJoystick(void) { currentJoystick=first; }
+    inline void lastJoystick(void) { currentJoystick=last; }
+
+    int nextJoystick(void);
+    int prevJoystick(void);
+
+    inline jsJoystick* getJoystick(int Joystick) 
+            { currentJoystick=Joystick; return js[Joystick]; }
+
+    inline jsJoystick* getJoystick(void) { return js[currentJoystick]; }
+    
+    inline int getCurrentJoystickId(void) { return currentJoystick; }
+};
+    
+#endif    
