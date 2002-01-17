ac75d9d46b05289790f3c5ff2e806c6980686fc4
curt <curt>
2002-01-17 16:22:29 +0000

    Handle newer versions of automake automatically. IRIX specific Makefile fixups.

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,49 @@
 #!/bin/sh
 
-echo "Running aclocal"
-aclocal
+OSTYPE=`uname -s`
+MACHINE=`uname -m`
+AUTO_MAKE_VERSION=`automake --version | head -1 | awk '{print $4}' | sed -e 's/\-p[0-9]$//' | sed -e 's/\.//'`
+# AM_CONDITIONAL(ANCIENT_AUTOMAKE, test $AUTO_MAKE_VERSION -lt 14)
+# AM_CONDITIONAL(OLD_AUTOMAKE, test $AUTO_MAKE_VERSION -lt 15)
+
+echo "Host info: $OSTYPE $MACHINE"
+echo -n " automake: `automake --version | head -1 | awk '{print $4}'`"
+echo " ($AUTO_MAKE_VERSION)"
+echo ""
+
+echo -n "Running aclocal"
+if [ $AUTO_MAKE_VERSION -ge 15 ]; then
+    echo " -I ."
+    aclocal -I .
+else
+    echo ""
+    aclocal
+fi
 
 echo "Running autoheader"
 autoheader
 
-echo "Running automake"
-automake --add-missing
+echo -n "Running automake"
+if [ $OSTYPE = "IRIX" -o $OSTYPE = "IRIX64" ]; then
+    echo " --add-missing --include-deps"
+    automake --add-missing --include-deps
+else
+    echo " --add-missing"
+    automake --add-missing
+fi
 
 echo "Running autoconf"
 autoconf
+
+# fixup Makefiles for Irix
+if test "$OS" = "IRIX" -o "$OS" = "IRIX64"; then
+    echo "Fixing Makefiles for Irix"
+    for n in `find . -name Makefile.in`; do \
+        mv -f $n $n.ar-new; \
+        sed 's/$(AR) cru /$(AR) -o /g' $n.ar-new > $n; \
+        rm -f $n.ar-new; \
+    done;
+fi
 
 echo ""
 echo "======================================"
