6c90295655e3744e4a59c4265c73b4ac421aed8c
tony <tony>
2002-02-22 13:27:19 +0000

    Adds flap cmd and pos to output

--- src/FDM/JSBSim/FGOutput.cpp
+++ src/FDM/JSBSim/FGOutput.cpp
@@ -148,9 +148,11 @@ void FGOutput::DelimitedOutput(string fname)
       outstream << "Aileron Cmd, ";
       outstream << "Elevator Cmd, ";
       outstream << "Rudder Cmd, ";
+      outstream << "Flap Cmd, ";
       outstream << "Aileron Pos, ";
       outstream << "Elevator Pos, ";
-      outstream << "Rudder Pos";
+      outstream << "Rudder Pos, ";
+      outstream << "Flap Pos"; 
     }
     if (SubSystems & ssRates) {
       outstream << ", ";
@@ -225,9 +227,11 @@ void FGOutput::DelimitedOutput(string fname)
     outstream << FCS->GetDaCmd() << ", ";
     outstream << FCS->GetDeCmd() << ", ";
     outstream << FCS->GetDrCmd() << ", ";
+    outstream << FCS->GetDfCmd() << ", ";
     outstream << FCS->GetDaPos() << ", ";
     outstream << FCS->GetDePos() << ", ";
-    outstream << FCS->GetDrPos();
+    outstream << FCS->GetDrPos() << ", ";
+    outstream << FCS->GetDfPos();
   }
   if (SubSystems & ssRates) {
     outstream << ", ";
