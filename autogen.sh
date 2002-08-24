63a8c79a145a3cc2d885cf8e6607ae26c93c74d1
curt <curt>
2002-08-24 05:54:33 +0000

    Brandon Bergren:
    2. I changed Simgear's autogen.sh so it is quieter, and errors out if
    autoheader or autoconf fails.  It puts all error output in autogen.err, and
    deletes it if nothing errored out.  The patch is in (sg.autogen.sh.patch.gz)
    This patch applies to flightgear, too.  If someone doesn't like what I did
    here, please speak up!  I really believe that this patch will help cause
    less confusion among people new to compiling flightgear.

--- autogen.sh
+++ autogen.sh
@@ -16,22 +16,33 @@ if [ $AUTO_MAKE_VERSION -ge 14 ]; then
     fi
 fi
 echo "Running aclocal $ACLOCAL_OPTS"
-aclocal $ACLOCAL_OPTS
+aclocal $ACLOCAL_OPTS 2> autogen.err
 
 echo "Running autoheader"
-autoheader
+autoheader 2>> autogen.err
+if [ ! -e src/Include/config.h.in ]; then
+    echo "ERROR: autoheader didn't create simgear/simgear_config.h.in!"
+    echo "Aborting ... consulte autogen.err for details."
+    exit 1
+fi    
 
 echo -n "Running automake"
 if [ $OSTYPE = "IRIX" -o $OSTYPE = "IRIX64" ]; then
     echo " --add-missing --include-deps"
-    automake --add-missing --include-deps
+    automake --add-missing --include-deps 2>> autogen.err
 else
     echo " --add-missing"
-    automake --add-missing
+    automake --add-missing 2>> autogen.err
 fi
 
 echo "Running autoconf"
-autoconf
+autoconf 2>> autogen.err
+
+if [ ! -e configure ]; then
+    echo "ERROR: configure was not created!"
+    echo "Aborting ... consulte autogen.err for details."
+    exit 1
+fi
 
 # fixup Makefiles for Irix
 if test "$OSTYPE" = "IRIX" -o "$OSTYPE" = "IRIX64"; then
@@ -55,3 +66,5 @@ fi
 
 echo "Now you are ready to run './configure'"
 echo "======================================"
+
+rm -f autogen.err
