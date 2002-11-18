80e8647330a957e9450daaef0ed9a7949a3b846f
curt <curt>
2002-11-18 21:34:07 +0000

    Removed depricated files.

--- src/GUI/apt_dlg.hxx
+++
@@ -1,7 +0,0 @@
-#include <plib/pu.h>
-
-extern void AptDialog_Cancel(puObject *);
-extern void AptDialog_OK (puObject *);
-extern void AptDialog_Reset(puObject *);
-extern void NewAirport(puObject *cb);
-extern void NewAirportInit(void);
