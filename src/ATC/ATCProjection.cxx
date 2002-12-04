25d9868759479b25c8a8b5fffeb28098013f376e
daveluff <daveluff>
2002-12-04 19:38:04 +0000

    Include GPL

--- src/ATC/ATCProjection.cxx
+++ src/ATC/ATCProjection.cxx
@@ -1,3 +1,23 @@
+// ATCProjection.cxx - A convienience projection class for the ATC/AI system.
+//
+// Written by David Luff, started 2002.
+//
+// Copyright (C) 2002  David C Luff - david.luff@nottingham.ac.uk
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 #include "ATCProjection.hxx"
 #include <math.h>
 #include <simgear/constants.h>
