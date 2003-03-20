c5c3095859c840afa9b7df90bebe24722f0c5668
ehofman <ehofman>
2003-03-20 08:51:01 +0000

    Add includes for MacOS X

--- src/MultiPlayer/multiplayrxmgr.cxx
+++ src/MultiPlayer/multiplayrxmgr.cxx
@@ -50,6 +50,7 @@
 *
 ******************************************************************/
 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
--- src/MultiPlayer/multiplaytxmgr.cxx
+++ src/MultiPlayer/multiplaytxmgr.cxx
@@ -30,6 +30,7 @@
 *
 ******************************************************************/
 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
