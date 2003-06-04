bfaaa63bb291c3abedfbda9d631f9a6288f3f964
curt <curt>
2003-06-04 20:27:05 +0000

    Changes in preparation for the 0.9.2 release.

--- src/Main/main.cxx
+++ src/Main/main.cxx
@@ -1603,7 +1603,7 @@ static bool fgMainInit( int argc, char **argv ) {
     fgInitFGRoot(argc, argv);
 
     // Check for the correct base package version
-    static char required_version[] = "0.9.1";
+    static char required_version[] = "0.9.2";
     string base_version = fgBasePackageVersion();
     if ( !(base_version == required_version) ) {
         // tell the operator how to use this application
