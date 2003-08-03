3d07fa8bda8a58853796e05ce1f7f5fd64ec9684
ehofman <ehofman>
2003-08-03 09:54:02 +0000

    Update the ambient, diffuse and specular scenery colors to match the real world more closely

--- src/Time/light.cxx
+++ src/Time/light.cxx
@@ -159,15 +159,15 @@ void fgLIGHT::Update( void ) {
 
     float *sun_color = thesky->get_sun_color();
 
-    scene_ambient[0] = fog_color[0] * ambient;
-    scene_ambient[1] = fog_color[1] * ambient;
-    scene_ambient[2] = fog_color[2] * ambient;
-    scene_ambient[3] = 1.0;
+    scene_ambient[0] = (sun_color[0]*0.1 + cloud_color[0]*0.9) * ambient;
+    scene_ambient[1] = (sun_color[1]*0.1 + cloud_color[1]*0.9) * ambient;
+    scene_ambient[2] = (sun_color[2]*0.1 + cloud_color[2]*0.9) * ambient;
+    scene_ambient[3] = 1.5;
 
-    scene_diffuse[0] = fog_color[0] * diffuse;
-    scene_diffuse[1] = fog_color[1] * diffuse;
-    scene_diffuse[2] = fog_color[2] * diffuse;
-    scene_diffuse[3] = 1.0;
+    scene_diffuse[0] = (sun_color[0]*0.25 + fog_color[0]*0.75) * diffuse;
+    scene_diffuse[1] = (sun_color[1]*0.25 + fog_color[1]*0.75) * diffuse;
+    scene_diffuse[2] = (sun_color[2]*0.25 + fog_color[2]*0.75) * diffuse;
+    scene_diffuse[3] = 1.5;
 
     scene_specular[0] = sun_color[0] * specular;
     scene_specular[1] = sun_color[1] * specular;
