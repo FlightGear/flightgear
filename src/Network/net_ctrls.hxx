de7d16cd273378b1757260e54d7e906bd665b71b
curt <curt>
2003-12-30 20:46:50 +0000

    Modified Files:  	net_ctrls.hxx net_fdm.hxx net_fdm_mini.hxx net_gui.hxx
    I, Curtis Olson, being the primary author of these header files, hereby
    release them into the public domain to facilitate interfacing FlightGear
    with other external code (which might need to remain proprietary or may
    be licensed under some not quite GPL compatible terms.)

--- src/Network/net_ctrls.hxx
+++ src/Network/net_ctrls.hxx
@@ -1,23 +1,9 @@
 // net_ctrls.hxx -- defines a common net I/O interface to the flight
 //                  sim controls
 //
-// Written by Curtis Olson, started July 2001.
+// Written by Curtis Olson - curt@flightgear.com, started July 2001.
 //
-// Copyright (C) 2001  Curtis L. Olson  - curt@flightgear.com
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 2 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// This file is in the Public Domain, and comes with no warranty.
 //
 // $Id$
 
