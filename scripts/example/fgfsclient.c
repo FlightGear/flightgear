b8ac6687196c80e447efbcda04e07180fd799b10
ehofman <ehofman>
2004-02-10 15:35:48 +0000

    Add some example remote controll scripts

---
+++ scripts/example/fgfsclient.c
@@ -0,0 +1,151 @@
+/* $Id$ */
+/* gcc -O2 -g -pedantic -Wall fgfsclient.c -o fgfsclient */
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdarg.h>
+#include <string.h>
+
+
+#define DFLTHOST	"localhost"
+#define DFLTPORT	5501
+#define MAXMSG		256
+#define fgfsclose	close
+
+
+void init_sockaddr(struct sockaddr_in *name, const char *hostname, unsigned port);
+int fgfswrite(int sock, char *msg, ...);
+const char *fgfsread(int sock, int wait);
+void fgfsflush(int sock);
+
+
+
+int fgfswrite(int sock, char *msg, ...)
+{
+	va_list va;
+	ssize_t len;
+	char buf[MAXMSG];
+
+	va_start(va, msg);
+	vsprintf(buf, msg, va);
+	va_end(va);
+	printf("SEND: \t<%s>\n", buf);
+	strcat(buf, "\015\012");
+
+	len = write(sock, buf, strlen(buf));
+	if (len < 0) {
+		perror("fgfswrite");
+		exit(EXIT_FAILURE);
+	}
+	return len;
+}
+
+
+
+const char *fgfsread(int sock, int timeout)
+{
+	static char buf[MAXMSG];
+	char *p;
+	fd_set ready;
+	struct timeval tv;
+	ssize_t len;
+
+	FD_ZERO(&ready);
+	FD_SET(sock, &ready);
+	tv.tv_sec = timeout;
+	tv.tv_usec = 0;
+	if (!select(32, &ready, 0, 0, &tv))
+		return NULL;
+
+	len = read(sock, buf, MAXMSG - 1);
+	if (len < 0) {
+		perror("fgfsread");
+		exit(EXIT_FAILURE);
+	} 
+	if (len == 0)
+		return NULL;
+
+	for (p = &buf[len - 1]; p >= buf; p--)
+		if (*p != '\015' && *p != '\012')
+			break;
+	*++p = '\0';
+	return strlen(buf) ? buf : NULL;
+}
+
+
+
+void fgfsflush(int sock)
+{
+	const char *p;
+	while ((p = fgfsread(sock, 0)) != NULL) {
+		printf("IGNORE: \t<%s>\n", p);
+	}
+}
+
+
+
+int fgfsconnect(const char *hostname, const int port)
+{
+	struct sockaddr_in serv_addr;
+	struct hostent *hostinfo;
+	int sock;
+
+	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock < 0) {
+		perror("fgfsconnect/socket");
+		return -1;
+	}
+
+	hostinfo = gethostbyname(hostname);
+	if (hostinfo == NULL) {
+		fprintf(stderr, "fgfsconnect: unknown host: \"%s\"\n", hostname);
+		close(sock);
+		return -2;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port);
+	serv_addr.sin_addr = *(struct in_addr *)hostinfo->h_addr;
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		perror("fgfsconnect/connect");
+		close(sock);
+		return -3;
+	}
+	return sock;
+}
+
+
+
+int main(int argc, char **argv)
+{
+	int sock;
+	unsigned port;
+	const char *hostname, *p;
+
+	hostname = argc > 1 ? argv[1] : DFLTHOST;
+	port = argc > 2 ? atoi(argv[2]) : DFLTPORT;
+
+	sock = fgfsconnect(hostname, port);
+	if (sock < 0)
+		return (EXIT_FAILURE);
+
+	fgfswrite(sock, "data");
+	fgfswrite(sock, "set /controls/engines/engine[%d]/throttle %d", 0, 1);
+	fgfswrite(sock, "get /sim/aircraft");
+	p = fgfsread(sock, 3);
+	if (p != NULL)
+		printf("READ: \t<%s>\n", p);
+	fgfswrite(sock, "quit");
+	fgfsclose(sock);
+	return EXIT_SUCCESS;
+}
+
+
---
+++ scripts/example/fgfsclient.cxx
@@ -0,0 +1,181 @@
+// $Id$
+// g++ -O2 -g -pedantic -Wall fgfsclient.cxx -o fgfsclient -lstdc++
+
+#include <errno.h>
+#include <iostream>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+const int maxlen = 256;
+
+
+class FGFSSocket {
+	int		sock;
+	bool		connected;
+	unsigned	timeout;
+    public:
+			FGFSSocket(const char *name, const unsigned port);
+			~FGFSSocket() { close(); };
+
+	int		write(const char *msg, ...);
+	const char 	*read(void);
+	inline void 	flush(void);
+	void		settimeout(unsigned t) { timeout = t; };
+    private:
+	int		close(void);
+};
+
+
+FGFSSocket::FGFSSocket(const char *hostname = "localhost", const unsigned port = 5501)
+	:
+	sock(-1),
+	connected(false),
+	timeout(1)
+{
+	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock < 0)
+		throw("FGFSSocket/socket");
+
+	struct hostent *hostinfo;
+	hostinfo = gethostbyname(hostname);
+	if (!hostinfo) {
+		close();
+		throw("FGFSSocket/gethostbyname: unknown host");
+	}
+
+	struct sockaddr_in serv_addr;
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port);
+	serv_addr.sin_addr = *(struct in_addr *)hostinfo->h_addr;
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		close();
+		throw("FGFSSocket/connect");
+	}
+	connected = true;
+	try {
+		write("data");
+	} catch (...) {
+		close();
+		throw;
+	}
+}
+
+
+int FGFSSocket::close(void)
+{
+	if (connected)
+		write("quit");
+	if (sock < 0)
+		return 0;
+	int ret = ::close(sock);
+	sock = -1;
+	return ret;
+}
+
+
+int FGFSSocket::write(const char *msg, ...)
+{
+	va_list va;
+	ssize_t len;
+	char buf[maxlen];
+	fd_set fd;
+	struct timeval tv;
+
+	FD_ZERO(&fd);
+	FD_SET(sock, &fd);
+	tv.tv_sec = timeout;
+	tv.tv_usec = 0;
+	if (!select(FD_SETSIZE, 0, &fd, 0, &tv))
+		throw("FGFSSocket::write/select: timeout exceeded");
+
+	va_start(va, msg);
+	vsprintf(buf, msg, va);
+	va_end(va);
+	std::cout << "SEND: " << buf << std::endl;
+	strcat(buf, "\015\012");
+
+	len = ::write(sock, buf, strlen(buf));
+	if (len < 0)
+		throw("FGFSSocket::write");
+	return len;
+}
+
+
+const char *FGFSSocket::read(void)
+{
+	static char buf[maxlen];
+	char *p;
+	fd_set fd;
+	struct timeval tv;
+	ssize_t len;
+
+	FD_ZERO(&fd);
+	FD_SET(sock, &fd);
+	tv.tv_sec = timeout;
+	tv.tv_usec = 0;
+	if (!select(FD_SETSIZE, &fd, 0, 0, &tv)) {
+		if (timeout == 0)
+			return 0;
+		else
+			throw("FGFSSocket::read/select: timeout exceeded");
+	}
+
+	len = ::read(sock, buf, maxlen - 1);
+	if (len < 0)
+		throw("FGFSSocket::read/read");
+	if (len == 0)
+		return 0;
+
+	for (p = &buf[len - 1]; p >= buf; p--)
+		if (*p != '\015' && *p != '\012')
+			break;
+	*++p = '\0';
+	return strlen(buf) ? buf : 0;
+}
+
+
+inline void FGFSSocket::flush(void)
+{
+	int i = timeout;
+	timeout = 0;
+	while (read())
+		;
+	timeout = i;
+}
+
+
+int main(const int argc, const char *argv[])
+try {
+	const char *hostname = argc > 1 ? argv[1] : "localhost";
+	int port = argc > 2 ? atoi(argv[2]) : 5501;
+
+	FGFSSocket f(hostname, port);
+	f.flush();
+	f.write("set /controls/engines/engine[%d]/throttle %lg", 0, 1.0);
+	f.write("set /controls/engines/engine[%d]/throttle %lg", 1, 1.0);
+	f.write("get /sim/aircraft");
+	const char *p = f.read();
+	if (p)
+		std::cout << "RECV: " << p << std::endl;
+	return 0;
+
+} catch (const char s[]) {
+	std::cerr << "Error: " << s << ": " << strerror(errno) << std::endl;
+	return -1;
+
+} catch (...) {
+	std::cerr << "Error: unknown exception" << std::endl;
+	return -1;
+}
+
+
+// vim:cindent
