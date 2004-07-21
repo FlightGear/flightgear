517a7a3e5ab51973848307b69018311dd8e933d5
curt <curt>
2004-07-21 21:49:54 +0000

    Updates for v0.9.5-pre2.

--- src/Main/main.cxx
+++ src/Main/main.cxx
@@ -1542,7 +1542,7 @@ bool fgMainInit( int argc, char **argv ) {
     fgInitFGRoot(argc, argv);
 
     // Check for the correct base package version
-    static char required_version[] = "0.9.5-pre1";
+    static char required_version[] = "0.9.5-pre2";
     string base_version = fgBasePackageVersion();
     if ( !(base_version == required_version) ) {
         // tell the operator how to use this application
