7857ef0381a70e9ae0b31cd16df1f08ea44aa8d7
curt <curt>
2004-07-29 21:54:15 +0000

    Tweaks for 0.9.5 release.

--- src/Main/main.cxx
+++ src/Main/main.cxx
@@ -1559,7 +1559,7 @@ bool fgMainInit( int argc, char **argv ) {
     fgInitFGRoot(argc, argv);
 
     // Check for the correct base package version
-    static char required_version[] = "0.9.5-pre3";
+    static char required_version[] = "0.9.5";
     string base_version = fgBasePackageVersion();
     if ( !(base_version == required_version) ) {
         // tell the operator how to use this application
