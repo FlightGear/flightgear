5ff3d39ad5564f7c2a776b5ef7f8a25a3b285554
ehofman <ehofman>
2004-09-19 17:02:16 +0000

    Shoot, this shouldn't have happened yet, but since it is, add the missing files also.

---
+++ src/Main/renderer.hxx
@@ -0,0 +1,33 @@
+
+#ifndef __FG_RENDERER_HXX
+#define __FG_RENDERER_HXX 1
+
+#include <simgear/screen/extensions.hxx>
+#include <simgear/scene/sky/sky.hxx>
+
+#define FG_ENABLE_MULTIPASS_CLOUDS 1
+
+class SGSky;
+extern SGSky *thesky;
+
+extern glPointParameterfProc glPointParameterfPtr;
+extern glPointParameterfvProc glPointParameterfvPtr;
+extern bool glPointParameterIsSupported;
+
+void fgRenderFrame();
+
+class FGRenderer {
+
+public:
+    FGRenderer() {}
+    ~FGRenderer() {}
+
+    void init();
+    void update(double dt);
+    void resize(int width, int height );
+
+    void screendump();
+    void build_states();
+};
+
+#endif
