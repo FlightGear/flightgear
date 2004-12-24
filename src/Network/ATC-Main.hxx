ec827732226e905a8a0ce9af700b63b183b18c5c
curt <curt>
2004-12-24 01:23:27 +0000

    More work on the ATC FS hardware interface.

---
+++ src/Network/ATC-Main.hxx
@@ -0,0 +1,98 @@
+// ATC-Main.hxx -- FGFS interface to ATC 610x hardware
+//
+// Written by Curtis Olson, started January 2002.
+//
+// Copyright (C) 2002  Curtis L. Olson - http://www.flightgear.org/~curt
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// $Id$
+
+
+#ifndef _FG_ATC_MAIN_HXX
+#define _FG_ATC_MAIN_HXX
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <plib/netChat.h>
+
+#include <Main/fg_props.hxx>
+
+#include "protocol.hxx"
+
+#include "ATC-Inputs.hxx"
+#include "ATC-Outputs.hxx"
+
+
+class FGATCMain : public FGProtocol {
+
+    FGATCInput *input0;         // board0 input interface class
+    FGATCInput *input1;         // board1 input interface class
+    FGATCOutput *output0;       // board0 output interface class
+    FGATCOutput *output1;       // board1 output interface class
+
+    SGPath input0_path;
+    SGPath input1_path;
+    SGPath output0_path;
+    SGPath output1_path;
+
+    int board;
+
+    int lock0_fd;
+    int lock1_fd;
+
+public:
+
+    FGATCMain() :
+        input0(NULL),
+        input1(NULL),
+        output0(NULL),
+        output1(NULL),
+        input0_path(""),
+        input1_path(""),
+        output0_path(""),
+        output1_path("")
+    { }
+
+    ~FGATCMain() {
+        delete input0;
+        delete input1;
+        delete output0;
+        delete output1;
+    }
+
+    // Open and initialize ATC 610x hardware
+    bool open();
+
+    void init_config();
+
+    bool process();
+
+    bool close();
+
+    inline void set_path_names( const SGPath &in0, const SGPath &in1,
+                                const SGPath &out0, const SGPath &out1 )
+    {
+        input0_path = in0;
+        input1_path = in1;
+        output0_path = out0;
+        output1_path = out1;
+    }
+};
+
+
+#endif // _FG_ATC_MAIN_HXX
---
+++ src/Network/ATC-Outputs.hxx
@@ -0,0 +1,90 @@
+// ATC-Outputs.hxx -- Translate FGFS properties into ATC hardware outputs
+//
+// Written by Curtis Olson, started November 2004.
+//
+// Copyright (C) 2004  Curtis L. Olson - http://www.flightgear.org/~curt
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// $Id$
+
+
+#ifndef _FG_ATC_OUTPUTS_HXX
+#define _FG_ATC_OUTPUTS_HXX
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <Main/fg_props.hxx>
+
+#define ATC_RADIO_DISPLAY_BYTES 48
+#define ATC_COMPASS_CH 5
+#define ATC_STEPPER_HOME 0xC0
+
+
+class FGATCOutput {
+
+    int is_open;
+
+    int board;
+    SGPath config;
+
+    int lock_fd;
+    int lamps_fd;
+    int radio_display_fd;
+    int stepper_fd;
+
+    char lamps_file[256];
+    char radio_display_file[256];
+    char stepper_file[256];
+
+    unsigned char radio_display_data[ATC_RADIO_DISPLAY_BYTES];
+
+    SGPropertyNode *lamps_out_node;
+    SGPropertyNode *radio_display_node;
+    SGPropertyNode *steppers_node;
+
+    void init_config();
+    bool do_lamps();
+    bool do_radio_display();
+    bool do_steppers();
+
+    // hardwired stepper motor code
+    float compass_position;
+
+public:
+
+    // Constructor: The _board parameter specifies which board to
+    // reference.  Possible values are 0 or 1.  The _config_file
+    // parameter specifies the location of the output config file (xml)
+    FGATCOutput( const int _board, const SGPath &_config_file );
+
+    // Destructor
+    ~FGATCOutput() { }
+
+    // need to pass in atc hardware lock_fd so that the radios and
+    // lamps can be blanked and so that the compass can be homed.
+    bool open( int lock_fd );
+
+    // process the hardware outputs.  This code assumes the calling
+    // layer will lock the hardware.
+    bool process();
+
+    bool close();
+};
+
+
+#endif // _FG_ATC_OUTPUTS_HXX
