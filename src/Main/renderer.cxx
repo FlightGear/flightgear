ac2f7d1e4ba9fe4ba894264d96250ceb4fe96f4d
mfranz <mfranz>
2005-10-04 18:01:45 +0000

    prevent view through big hole in carrier deck

--- src/Main/renderer.cxx
+++ src/Main/renderer.cxx
@@ -498,7 +498,7 @@ FGRenderer::update( bool refresh_camera_settings ) {
         current_aircraft.fdm_state->get_Altitude() * SG_FEET_TO_METER
         - cur_fdm_state->get_Runway_altitude_m();
 
-    if ( agl > 10.0 ) {
+    if ( agl > 50.0 ) {
         scene_nearplane = 10.0f;
         scene_farplane = 120000.0f;
     } else {
