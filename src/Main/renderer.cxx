a4bbb31927e481adecfacd21575bfb71c47ae005
mfranz <mfranz>
2006-01-28 00:10:39 +0000

    Resizing is done when fg_os{,_sdl}.cxx says so, not in every single frame. (This defeats the purpose of the resize callback and makes "listening" to the xsize useless.)

--- src/Main/renderer.cxx
+++ src/Main/renderer.cxx
@@ -336,10 +336,6 @@ FGRenderer::update( bool refresh_camera_settings ) {
     current__view->set_dirty();
 
     if ( refresh_camera_settings ) {
-        // update view port
-        resize( fgGetInt("/sim/startup/xsize"),
-                fgGetInt("/sim/startup/ysize") );
-
         // Tell GL we are switching to model view parameters
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
