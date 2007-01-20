00a2fb10549d645d01726363b9928c1584d69331
mfranz <mfranz>
2007-01-20 22:45:50 +0000

    add fgSetString() stub

--- src/FDM/YASim/yasim-test.cpp
+++ src/FDM/YASim/yasim-test.cpp
@@ -13,6 +13,7 @@ using namespace yasim;
 bool fgSetFloat (const char * name, float val) { return false; }
 bool fgSetBool(char const * name, bool val) { return false; }
 bool fgGetBool(char const * name, bool def) { return false; }
+bool fgSetString(char const * name, char const * str) { return false; }
 SGPropertyNode* fgGetNode (const char * path, bool create) { return 0; }
 SGPropertyNode* fgGetNode (const char * path, int i, bool create) { return 0; }
 float fgGetFloat (const char * name, float defaultValue) { return 0; }
