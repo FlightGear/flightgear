2105d6744da32a817045625430afe7ba2c464e9b
mfranz <mfranz>
2007-01-21 10:48:22 +0000

    add fgSetString() stub

--- src/FDM/YASim/proptest.cpp
+++ src/FDM/YASim/proptest.cpp
@@ -16,6 +16,7 @@ class SGPropertyNode;
 bool fgSetFloat (const char * name, float val) { return false; }
 bool fgSetBool(char const * name, bool val) { return false; }
 bool fgGetBool(char const * name, bool def) { return false; }
+bool fgSetString(char const * name, char const * str) { return false; }
 SGPropertyNode* fgGetNode (const char * path, bool create) { return 0; }
 SGPropertyNode* fgGetNode (const char * path, int i, bool create) { return 0; }
 float fgGetFloat (const char * name, float defaultValue) { return 0; }
