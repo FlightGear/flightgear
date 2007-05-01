2f66ea93dcd313e40adce97d9435df146d79b5b5
mfranz <mfranz>
2007-05-01 09:18:13 +0000

    since the submodel_mgr subsystem does now also hold references to AI models, we need to explicitly destroy that here, too, so that it has guaranteed access to the Nasal subsystem. Otherwise we get a segfault on exit. When the next subsystem needs this special treatement (radar?), we should introduce a new subsystem group (in addition to INIT and GENERAL)

--- src/Main/util.cxx
+++ src/Main/util.cxx
@@ -106,8 +106,14 @@ fgExit (int status)
 {
     // remove subsystems first, which need access to other subsystems in their
     // destructors (e.g. "nasal")
-    SGSubsystem *sub = globals->get_subsystem("ai_model");
+    SGSubsystem *sub;
+
+    sub = globals->get_subsystem("ai_model");
     globals->get_subsystem_mgr()->get_group(SGSubsystemMgr::GENERAL)->remove_subsystem("ai_model");
+    delete sub;
+
+    sub = globals->get_subsystem("submodel_mgr");
+    globals->get_subsystem_mgr()->get_group(SGSubsystemMgr::GENERAL)->remove_subsystem("submodel_mgr");
     delete sub;
 
     SG_LOG(SG_GENERAL, SG_INFO, "Exiting FlightGear with status " << status);
