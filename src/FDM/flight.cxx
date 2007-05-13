ab2209be8c10b3e0cec7e0930a1bcb9eed8e3351
mfranz <mfranz>
2007-05-13 11:53:21 +0000

    s/resistence/resistance/

--- src/FDM/flight.cxx
+++ src/FDM/flight.cxx
@@ -873,7 +873,7 @@ FGInterface::get_agl_m(double t, const double pt[3],
   sgdCopyVec3(normal, _normal.data());
   sgdCopyVec3(vel, _vel.data());
   if (material) {
-    *loadCapacity = material->get_load_resistence();
+    *loadCapacity = material->get_load_resistance();
     *frictionFactor = material->get_friction_factor();
 
   } else {
@@ -920,7 +920,7 @@ FGInterface::get_agl_ft(double t, const double pt[3],
   // return material properties if available
   if (material) {
     // FIXME: convert units?? now pascal to lbf/ft^2
-    *loadCapacity = 0.020885434*material->get_load_resistence();
+    *loadCapacity = 0.020885434*material->get_load_resistance();
     *frictionFactor = material->get_friction_factor();
   } else {
     *loadCapacity = DBL_MAX;
