cbcc7af5d0efb8a5d18d527bf10b0085acb253c6
mfranz <mfranz>
2007-07-13 10:15:48 +0000

    move the other parts of _init_keyboard() to _postinit_keyboard(), too. This is still before anything else than the splash screen is displayed, and it's cleaner that way. (The Nasal processing parts *must* be there.)

--- src/Input/input.hxx
+++ src/Input/input.hxx
@@ -245,12 +245,6 @@ private:
 
 
   /**
-   * Initialize key bindings.
-   */
-  void _init_keyboard ();
-
-
-  /**
    * Initialize joystick bindings.
    */
   void _init_joystick ();
@@ -277,8 +271,8 @@ private:
 			    const string name);
 
   /**
-   * Initialize nasal parts that had to wait for the nasal to get
-   * functional.
+   * Initialize key bindings, as well as those joystick parts that
+   * depend on a working Nasal subsystem.
    */
   void _postinit_keyboard ();
   void _postinit_joystick ();
