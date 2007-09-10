ec8be8c67a32518a1e74afddb7d8546b7b38e6bb
timoore <timoore>
2007-09-10 05:08:57 +0000

    Added copyright to ground radar files

--- src/Instrumentation/groundradar.cxx
+++ src/Instrumentation/groundradar.cxx
@@ -1,6 +1,6 @@
 //  groundradar.cxx - Background layer for the ATC radar.
 //
-//  Written by Csaba Halasz.
+//  Copyright (C) 2007 Csaba Halasz.
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License as
