b3cb1407e96a0762526514a299f3bd4f05f5a9da
durk <durk>
2007-12-11 21:32:50 +0000

    Release related changes:  - New version number  - New aircraft selection for base package  - Make sure flightgear checks for the right version of the    base package.

--- src/Main/main.cxx
+++ src/Main/main.cxx
@@ -1053,7 +1053,7 @@ bool fgMainInit( int argc, char **argv ) {
     fgInitFGRoot(argc, argv);
 
     // Check for the correct base package version
-    static char required_version[] = "0.9.11";
+    static char required_version[] = "1.0.0";
     string base_version = fgBasePackageVersion();
     if ( !(base_version == required_version) ) {
         // tell the operator how to use this application
