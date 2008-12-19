231225da67eeca00112592b87e9ae6632cfc2251
curt <curt>
2008-12-19 20:41:57 +0000

    Attempt to sort out the version number mess in preparation for the v1.9.0 release.

--- src/Main/main.cxx
+++ src/Main/main.cxx
@@ -1016,7 +1016,7 @@ bool fgMainInit( int argc, char **argv ) {
     fgInitFGRoot(argc, argv);
 
     // Check for the correct base package version
-    static char required_version[] = "1.99.5";
+    static char required_version[] = "1.9.0";
     string base_version = fgBasePackageVersion();
     if ( !(base_version == required_version) ) {
         // tell the operator how to use this application
