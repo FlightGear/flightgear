f0b143778fe395b147a690ba41d536eb118472b5
jmt <jmt>
2009-11-14 11:10:34 +0000

    Dave Perry:
    Allows using a nasal filter to smoothly park the gs needle when out
    of range or frequency changes to a station w/o gs.

--- src/Instrumentation/navradio.cxx
+++ src/Instrumentation/navradio.cxx
@@ -580,6 +580,8 @@ void FGNavRadio::updateGlideSlope(double dt, const SGVec3d& aircraft, double sig
   if (!_gs || !inrange_node->getBoolValue()) {
     gs_dist_node->setDoubleValue( 0.0 );
     gs_inrange_node->setBoolValue(false);
+    _gsNeedleDeflection = 0.0;
+    _gsNeedleDeflectionNorm = 0.0;
     return;
   }
   
@@ -589,6 +591,8 @@ void FGNavRadio::updateGlideSlope(double dt, const SGVec3d& aircraft, double sig
   gs_inrange_node->setBoolValue(gsInRange);
         
   if (!gsInRange) {
+    _gsNeedleDeflection = 0.0;
+    _gsNeedleDeflectionNorm = 0.0;
     return;
   }
   
