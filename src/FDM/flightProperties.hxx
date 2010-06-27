14be21ea5ce0f5a2b9234490e8ad891e8b7f43b6
James Turner <zakalawe@mac.com>
2010-06-27 20:34:00 +0100

    Add license / copyright info to newly added files.

--- src/FDM/flightProperties.hxx
+++ src/FDM/flightProperties.hxx
@@ -1,3 +1,25 @@
+// flightProperties.hxx -- expose FDM properties via helper methods
+//
+// Written by James Turner, started June 2010.
+//
+// Copyright (C) 2010  Curtis L. Olson  - http://www.flightgear.org/~curt
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// $Id$
+
 #ifndef FG_FLIGHT_PROPERTIES_HXX
 #define FG_FLIGHT_PROPERTIES_HXX
 
