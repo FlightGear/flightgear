f969504e6b0e88af72ff383dbad2406258fcd6a0
James Turner <zakalawe@mac.com>
2010-09-29 22:27:27 +0100

    Tidy fg_commands header.

--- src/Main/fg_commands.hxx
+++ src/Main/fg_commands.hxx
@@ -3,23 +3,11 @@
 #ifndef __FG_COMMANDS_HXX
 #define __FG_COMMANDS_HXX
 
-#ifndef __cplusplus
-# error This library requires C++
-#endif
-
-
-#ifdef HAVE_CONFIG_H
-#  include <config.h>
-#endif
-
-#include <simgear/structure/commands.hxx>
-#include <simgear/props/props.hxx>
-
 /**
  * Initialize the built-in commands.
  */
 void fgInitCommands ();
 
-#endif
-
 // end of fg_commands.hxx
+
+#endif
