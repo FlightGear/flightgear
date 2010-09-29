5000d212ec18ff6c1d76605eb55914e81ee2432f
Csaba Halasz <Csaba.Halasz@gmail.com>
2010-09-30 01:05:04 +0200

    Merged commit from Vivian Meazza: Rationalize methods (ea85e50c2093942164bbc02f371a0c05b259f9f4)

--- src/AIModel/AIEscort.cxx
+++ src/AIModel/AIEscort.cxx
@@ -230,7 +230,6 @@ bool FGAIEscort::getGroundElev(SGGeod inpos) {
 
 }
 
-
 void FGAIEscort::setParent()
 {
     double lat = _selected_ac->getDoubleValue("position/latitude-deg");
@@ -267,6 +266,22 @@ void FGAIEscort::setParent()
 
     _relbrg = calcRelBearingDeg(_tgtbrg, hdg);
 
+}
+
+void FGAIEscort::calcRangeBearing(double lat, double lon, double lat2, double lon2,
+                                  double &range, double &bearing) const
+{
+    // calculate the bearing and range of the second pos from the first
+    double az2, distance;
+    geo_inverse_wgs_84(lat, lon, lat2, lon2, &bearing, &az2, &distance);
+    range = distance * SG_METER_TO_NM;
+}
+
+double FGAIEscort::calcTrueBearingDeg(double bearing, double heading)
+{
+    double angle = bearing + heading;
+    SG_NORMALIZE_RANGE(angle, 0.0, 360.0);
+    return angle;
 }
 
 SGVec3d FGAIEscort::getCartHitchPosAt(const SGVec3d& _off) const {
