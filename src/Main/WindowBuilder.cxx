FlightGear/flightgear

commit 863551a932e21f3d4315c0ceb51005929acc9f20

fix #142: initial window position Be (at least a bit) smarter with initial x/y position Also some minor type/comment issues.

src/Main/WindowBuilder.cxx
@@ -69,9 +69,9 @@ WindowBuilder::makeDefaultTraits(bool stencil)
     traits->red = traits->green = traits->blue = cbits;
     traits->depth = zbits;
     if (alpha)
-	traits->alpha = 8;
+        traits->alpha = 8;
     if (stencil)
-	traits->stencil = 8;
+        traits->stencil = 8;
     traits->doubleBuffer = true;
     traits->mipMapGeneration = true;
     traits->windowName = "FlightGear";
@@ -83,20 +83,21 @@ WindowBuilder::makeDefaultTraits(bool stencil)
         unsigned width = 0;
         unsigned height = 0;
         wsi->getScreenResolution(*traits, width, height);
-	traits->windowDecoration = false;
+        traits->windowDecoration = false;
         traits->width = width;
         traits->height = height;
         traits->supportsResize = false;
     } else {
-	traits->windowDecoration = true;
+        traits->windowDecoration = true;
         traits->width = w;
         traits->height = h;
-#if defined(WIN32) || defined(__APPLE__)
+        unsigned screenwidth = 0;
+        unsigned screenheight = 0;
+        wsi->getScreenResolution(*traits, screenwidth, screenheight);
         // Ugly Hack, why does CW_USEDEFAULT works like phase of the moon?
         // Mac also needs this to show window frame, menubar and Docks
-        traits->x = 100;
-        traits->y = 100;
-#endif
+        traits->x = (w>screenwidth) ? 0 : (screenwidth-w)/3;
+        traits->y = (h>screenheight) ? 0 : (screenheight-h)/3;
         traits->supportsResize = true;
     }
     return traits;

src/Main/options.cxx
@@ -1838,7 +1838,7 @@ fgUsage (bool verbose)
 
                          while ( t_str.size() > 47 ) {
 
-                            unsigned int m = t_str.rfind(' ', 47);
+                            string::size_type m = t_str.rfind(' ', 47);
                             msg += t_str.substr(0, m) + '\n';
                             msg.append( 32, ' ');
 

src/Main/viewmgr.cxx
@@ -356,7 +356,7 @@ FGViewMgr::update (double dt)
   abs_viewer_position = loop_view->getViewPosition();
 
   // update audio listener values
-  // set the viewer posotion in Cartesian coordinates in meters
+  // set the viewer position in Cartesian coordinates in meters
   smgr->set_position( abs_viewer_position, loop_view->getPosition() );
   smgr->set_orientation( current_view_orientation );
 

src/MultiPlayer/mpmessages.hxx
@@ -164,7 +164,7 @@ struct FGExternalMotionData {
   // the earth centered frame
   SGVec3f angularAccel;
   
-  // The set of properties recieved for this timeslot
+  // The set of properties received for this timeslot
   std::vector<FGPropertyData*> properties;
 
   ~FGExternalMotionData()

src/Sound/sample_queue.cxx
@@ -80,7 +80,7 @@ FGSampleQueue::update (double dt)
             last_volume = volume;
         }
 
-        // process mesage queue
+        // process message queue
         const string msgid = "Sequential Audio Message";
         bool now_playing = false;
         if ( exists( msgid ) ) {

src/Time/light.cxx
@@ -345,7 +345,7 @@ void FGLight::update_adj_fog_color () {
     else
        hor_rotation = fmod(hor_rotation, SGD_2PI);
 
-    // revert to unmodified values before usign them.
+    // revert to unmodified values before using them.
     //
     SGVec4f color = thesky->get_scene_color();
 
@@ -359,7 +359,7 @@ void FGLight::update_adj_fog_color () {
     float s_green = color[1]*color[1]*color[1];
     float s_blue =  color[2]*color[2];
 
-    // interpolate beween the sunrise/sunset color and the color
+    // interpolate between the sunrise/sunset color and the color
     // at the opposite direction of this effect. Take in account
     // the current visibility.
     //

commit ba7d8b8155731c95acf43f5e5a5a007cc6e96cdc

Ensure window position is 0,0 for multi-display setups with fullscreen mode Fix an issue introduced by commit 863551a932e21f3d4315c0ceb51005929acc9f20 for multi-display setups. Some code clean-up.

src/Main/WindowBuilder.cxx
@@ -52,26 +52,30 @@ WindowBuilder::makeDefaultTraits(bool stencil)
 {
     GraphicsContext::WindowingSystemInterface* wsi
         = osg::GraphicsContext::getWindowingSystemInterface();
-    int w = fgGetInt("/sim/startup/xsize");
-    int h = fgGetInt("/sim/startup/ysize");
-    int bpp = fgGetInt("/sim/rendering/bits-per-pixel");
-    bool alpha = fgGetBool("/sim/rendering/clouds3d-enable");
-    bool fullscreen = fgGetBool("/sim/startup/fullscreen");
-
     GraphicsContext::Traits* traits = new osg::GraphicsContext::Traits;
     traits->readDISPLAY();
+
+    unsigned screenwidth = 0;
+    unsigned screenheight = 0;
+    wsi->getScreenResolution(*traits, screenwidth, screenheight);
+
     if (traits->displayNum < 0)
         traits->displayNum = 0;
     if (traits->screenNum < 0)
         traits->screenNum = 0;
+
+    int bpp = fgGetInt("/sim/rendering/bits-per-pixel");
+    bool alpha = fgGetBool("/sim/rendering/clouds3d-enable");
     int cbits = (bpp <= 16) ?  5 :  8;
     int zbits = (bpp <= 16) ? 16 : 24;
     traits->red = traits->green = traits->blue = cbits;
     traits->depth = zbits;
     if (alpha)
         traits->alpha = 8;
+
     if (stencil)
         traits->stencil = 8;
+
     traits->doubleBuffer = true;
     traits->mipMapGeneration = true;
     traits->windowName = "FlightGear";
@@ -79,26 +83,25 @@ WindowBuilder::makeDefaultTraits(bool stencil)
     traits->sampleBuffers = fgGetBool("/sim/rendering/multi-sample-buffers", traits->sampleBuffers);
     traits->samples = fgGetInt("/sim/rendering/multi-samples", traits->samples);
     traits->vsync = fgGetBool("/sim/rendering/vsync-enable", traits->vsync);
-    if (fullscreen) {
-        unsigned width = 0;
-        unsigned height = 0;
-        wsi->getScreenResolution(*traits, width, height);
-        traits->windowDecoration = false;
-        traits->width = width;
-        traits->height = height;
+    traits->windowDecoration = !fgGetBool("/sim/startup/fullscreen");
+
+    if (!traits->windowDecoration) {
+        // fullscreen
         traits->supportsResize = false;
+        traits->width = screenwidth;
+        traits->height = screenheight;
     } else {
-        traits->windowDecoration = true;
+        // window
+        int w = fgGetInt("/sim/startup/xsize");
+        int h = fgGetInt("/sim/startup/ysize");
+        traits->supportsResize = true;
         traits->width = w;
         traits->height = h;
-        unsigned screenwidth = 0;
-        unsigned screenheight = 0;
-        wsi->getScreenResolution(*traits, screenwidth, screenheight);
-        // Ugly Hack, why does CW_USEDEFAULT works like phase of the moon?
-        // Mac also needs this to show window frame, menubar and Docks
-        traits->x = (w>screenwidth) ? 0 : (screenwidth-w)/3;
-        traits->y = (h>screenheight) ? 0 : (screenheight-h)/3;
-        traits->supportsResize = true;
+        if ((w>0)&&(h>0))
+        {
+            traits->x = ((unsigned)w>screenwidth) ? 0 : (screenwidth-w)/3;
+            traits->y = ((unsigned)h>screenheight) ? 0 : (screenheight-h)/3;
+        }
     }
     return traits;
 }
@@ -166,8 +169,11 @@ GraphicsWindow* WindowBuilder::buildWindow(const SGPropertyNode* winNode)
     int traitsSet = setFromProperty(traits->hostName, winNode, "host-name");
     traitsSet |= setFromProperty(traits->displayNum, winNode, "display");
     traitsSet |= setFromProperty(traits->screenNum, winNode, "screen");
+
     const SGPropertyNode* fullscreenNode = winNode->getNode("fullscreen");
+
     if (fullscreenNode && fullscreenNode->getBoolValue()) {
+        // fullscreen mode
         unsigned width = 0;
         unsigned height = 0;
         wsi->getScreenResolution(*traits, width, height);
@@ -175,9 +181,16 @@ GraphicsWindow* WindowBuilder::buildWindow(const SGPropertyNode* winNode)
         traits->width = width;
         traits->height = height;
         traits->supportsResize = false;
+        traits->x = 0;
+        traits->y = 0;
         traitsSet = 1;
     } else {
         int resizable = 0;
+        if (fullscreenNode && !fullscreenNode->getBoolValue())
+        {
+            traits->windowDecoration = true;
+            resizable = 1;
+        }
         resizable |= setFromProperty(traits->windowDecoration, winNode,
                                      "decoration");
         resizable |= setFromProperty(traits->width, winNode, "width");