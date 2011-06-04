bc9ffe96c2e1c175c6188d1c96771d84ef5cbdd1
ThorstenB <brehmt@gmail.com>
2011-06-04 17:55:37 +0200

    Minor error message formatting.

--- src/Main/main.cxx
+++ src/Main/main.cxx
@@ -618,10 +618,11 @@ int fgMainInit( int argc, char **argv ) {
         // tell the operator how to use this application
 
         SG_LOG( SG_GENERAL, SG_ALERT, "" ); // To popup the console on windows
-        cerr << endl << "Base package check failed ... " \
-             << "Found version " << base_version << " at: " \
-             << globals->get_fg_root() << endl;
-        cerr << "Please upgrade to version: " << required_version << endl;
+        cerr << endl << "Base package check failed:" << endl \
+             << "  Version " << base_version << " found at: " \
+             << globals->get_fg_root() << endl \
+             << "  Version " << required_version << " is required." << endl \ 
+             << "Please upgrade/downgrade base package." << endl;
 #ifdef _MSC_VER
         cerr << "Hit a key to continue..." << endl;
         cin.get();
