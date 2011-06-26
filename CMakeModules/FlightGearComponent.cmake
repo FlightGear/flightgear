7abf7c57482da1d2df8bb47036b83af444bad68c
James Turner <zakalawe@mac.com>
2011-06-26 07:49:37 +0100

    CMake header support for XCode/Visual Studio

--- CMakeModules/FlightGearComponent.cmake
+++ CMakeModules/FlightGearComponent.cmake
@@ -2,7 +2,7 @@
 macro(flightgear_component name sources)
 
 	set(libName "fg${name}")
-	add_library(${libName} STATIC ${sources} )
+	add_library(${libName} STATIC ${sources} ${ARGV2})
 
 	set_property(GLOBAL APPEND PROPERTY FG_LIBS ${libName})
     
