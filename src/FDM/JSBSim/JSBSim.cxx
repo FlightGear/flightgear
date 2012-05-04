0f462d4a6e8ac37126dc91b52999e6bf47f5f17d
ThorstenB <brehmt@gmail.com>
2012-05-05 01:42:41 +0200

    Fix rpmlint/Linux packager complaints about outdated/invalid FSF address in some license headers.

--- src/FDM/JSBSim/JSBSim.cxx
+++ src/FDM/JSBSim/JSBSim.cxx
@@ -16,7 +16,7 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
 //
 // $Id: JSBSim.cxx,v 1.64 2010/10/31 04:49:25 jberndt Exp $
 
