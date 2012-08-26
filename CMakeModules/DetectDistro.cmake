f001c5f9fe37dd9bd600180e9d058d3880b6523d
Christian Schmitt <chris@ilovelinux.de>
2012-08-26 11:55:45 +0200

    DetectDistro.cmake: Correct the lsb_release name

--- CMakeModules/DetectDistro.cmake
+++ CMakeModules/DetectDistro.cmake
@@ -4,7 +4,7 @@ message(STATUS "System is: ${CMAKE_SYSTEM_NAME}")
 
 if(CMAKE_SYSTEM_NAME MATCHES "Linux")
     # Detect Linux distribution (if possible)
-    execute_process(COMMAND "/usr/bin/lsb-release" "-is"
+    execute_process(COMMAND "/usr/bin/lsb_release" "-is"
                     TIMEOUT 4
                     OUTPUT_VARIABLE LINUX_DISTRO
                     ERROR_QUIET
