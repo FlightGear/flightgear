db53e4c0b860c7242a20c704982a20ab4c356594
Frederic Bouvier <fredfgfs01@free.fr>
2012-09-16 19:26:12 +0200

    Sort fgfs source files in IDE folders

--- CMakeModules/FlightGearComponent.cmake
+++ CMakeModules/FlightGearComponent.cmake
@@ -1,12 +1,19 @@
 
 macro(flightgear_component name sources)
+    set(fc ${name})
+    set(fh ${name})
     foreach(s ${sources})
         set_property(GLOBAL
             APPEND PROPERTY FG_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/${s}")
+        set(fc "${fc}#${CMAKE_CURRENT_SOURCE_DIR}/${s}")
     endforeach()
 
-	foreach(h ${ARGV2})
-		set_property(GLOBAL
-			APPEND PROPERTY FG_HEADERS "${CMAKE_CURRENT_SOURCE_DIR}/${h}")
-	endforeach()
+    foreach(h ${ARGV2})
+        set_property(GLOBAL
+            APPEND PROPERTY FG_HEADERS "${CMAKE_CURRENT_SOURCE_DIR}/${h}")
+        set(fh "${fh}#${CMAKE_CURRENT_SOURCE_DIR}/${h}")
+    endforeach()
+
+    set_property(GLOBAL APPEND PROPERTY FG_GROUPS_C "${fc}@")
+    set_property(GLOBAL APPEND PROPERTY FG_GROUPS_H "${fh}@")
 endmacro()
