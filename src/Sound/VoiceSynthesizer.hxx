19aedd45231973ab42a1f02a95c7fa0c0785f36a
Torsten Dreyer <torsten@t3r.de>
2014-05-06 10:33:50 +0200

    Add GPL header

--- src/Sound/VoiceSynthesizer.hxx
+++ src/Sound/VoiceSynthesizer.hxx
@@ -1,10 +1,21 @@
 /*
- * VoiceSynthesizer.hxx
- *
- *  Created on: Apr 24, 2014
- *      Author: flightgear
+ * VoiceSynthesizer.hxx - wraps flite+hts_engine
+ * Copyright (C) 2014  Torsten Dreyer - torsten (at) t3r (dot) de
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 #ifndef VOICESYNTHESIZER_HXX_
 #define VOICESYNTHESIZER_HXX_
 
