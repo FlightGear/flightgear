263bc56b4ecfa08e08a6f116f5b6a52231dfa74b
James Turner <zakalawe@mac.com>
2014-11-13 15:15:52 +0000

    FreeBSD fixes.
    From:
    http://svnweb.freebsd.org/ports/head/games/flightgear/files/

--- 3rdparty/hts_engine_API/lib/HTS_misc.c
+++ 3rdparty/hts_engine_API/lib/HTS_misc.c
@@ -245,7 +245,7 @@ size_t HTS_ftell(HTS_File * fp)
    } else if (fp->type == HTS_FILE) {
       fpos_t pos;
       fgetpos((FILE *) fp->pointer, &pos);
-#if defined(_WIN32) || defined(__CYGWIN__) || defined(__APPLE__) || defined(__ANDROID__)
+#if defined(_WIN32) || defined(__CYGWIN__) || defined(__APPLE__) || defined(__ANDROID__) || defined(__FreeBSD__)
       return (size_t) pos;
 #else
       return (size_t) pos.__pos;
