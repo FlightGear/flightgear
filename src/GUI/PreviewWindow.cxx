8fdb56a1854ae9587f8c54ae3d5dde6c3d162351
James Turner <zakalawe@mac.com>
2017-07-19 13:21:06 +0100

    Only qInfo was added in 5.5
    Other logging operators are present in 5.0, so simplify this code.

--- src/GUI/PreviewWindow.cxx
+++ src/GUI/PreviewWindow.cxx
@@ -7,9 +7,6 @@
 
 #if QT_VERSION < QT_VERSION_CHECK(5, 5, 0)
 #define qInfo       qDebug
-#define qWarning    qDebug
-#define qFatal      qDebug
-#define qCritical   qDebug
 #endif
 
 const int BORDER_SIZE = 16;
