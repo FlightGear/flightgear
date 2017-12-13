FlightGear/flightgear

commit 0dbb0dff9e238498475c52281a867e8408c72a84

Three Nasal properties of waypoint objects now accessible: airport, runway, navaid
Waypoint objects used in Nasal code can now return their airport,
runway or navaid object (Nasal ghost). More precisely:
  - if waypoint 'wpt' was made from an airport object[1], then
    'wpt.airport' is this airport object;
  - if waypoint 'wpt' was made from a runway object[2], then
    'wpt.runway' is this runway object, and 'wpt.airport' is the
    airport containing that runway;
  - if waypoint 'wpt' was made from a navaid object[3], then
    'wpt.navaid' is this navaid object.

When one of the three properties 'airport', 'runway', and 'navaid' is
not applicable to a given waypoint due to the type of the underlying
FGPositioned, its value is nil.

The code for these properties was already mostly there, but
unreachable from Nasal.

[1] For instance, with createWPFrom(airportinfo("LOWI"))

[2] For instance, with createWPFrom(airportinfo("LOWI").runway("26"))

[3] For instance, with:

      var apt = airportinfo("LOWI");
      var navaid = findNavaidByFrequencyMHz(apt, 109.7);
      var navaidWpt = createWPFrom(navaid);

src/Scripting/NasalPositioned.cxx
@@ -22,7 +22,7 @@
 #  include "config.h"
 #endif
 
-#include <string.h>
+#include <cstring>
 
 #include "NasalPositioned.hxx"
 
@@ -429,6 +429,45 @@ static const char* airportGhostGetMember(naContext c, void* g, naRef field, naRe
   return "";
 }
 
+// Return the navaid ghost associated with a waypoint of navaid type.
+static naRef waypointNavaid(naContext c, Waypt* wpt)
+{
+  FGPositioned* pos = wpt->source();
+
+  if (!FGPositioned::isNavaidType(pos)) {
+    return naNil();
+  }
+
+  return ghostForNavaid(c, static_cast<FGNavRecord*>(pos));
+}
+
+// Return the airport ghost associated with a waypoint of airport or runway
+// type.
+static naRef waypointAirport(naContext c, Waypt* wpt)
+{
+  FGPositioned* pos = wpt->source();
+
+  if (FGPositioned::isRunwayType(pos)) {
+    pos = static_cast<FGRunway*>(pos) -> airport();
+  } else if (!FGPositioned::isAirportType(pos)) {
+    return naNil();
+  }
+
+  return ghostForAirport(c, static_cast<FGAirport*>(pos));
+}
+
+// Return the runway ghost associated with a waypoint of runway type.
+static naRef waypointRunway(naContext c, Waypt* wpt)
+{
+  FGPositioned* pos = wpt->source();
+
+  if (!FGPositioned::isRunwayType(pos)) {
+    return naNil();
+  }
+
+  return ghostForRunway(c, static_cast<FGRunway*>(pos));
+}
+
 static const char* waypointCommonGetMember(naContext c, Waypt* wpt, const char* fieldName, naRef* out)
 {
   if (!strcmp(fieldName, "wp_name") || !strcmp(fieldName, "id")) *out = stringToNasal(c, wpt->ident());
@@ -450,8 +489,14 @@ static const char* waypointCommonGetMember(naContext c, Waypt* wpt, const char* 
     }
   } else if (!strcmp(fieldName, "heading_course")) {
       *out = naNum(wpt->headingRadialDeg());
+  } else if (!strcmp(fieldName, "navaid")) {
+    *out = waypointNavaid(c, wpt);
+  } else if (!strcmp(fieldName, "airport")) {
+    *out = waypointAirport(c, wpt);
+  } else if (!strcmp(fieldName, "runway")) {
+    *out = waypointRunway(c, wpt);
   } else {
-    return NULL; // member not found
+    return nullptr; // member not found
   }
 
   return "";
@@ -2636,65 +2681,6 @@ static naRef f_leg_courseAndDistanceFrom(naContext c, naRef me, int argc, naRef*
     return result;
 }
 
-static naRef f_waypoint_navaid(naContext c, naRef me, int argc, naRef* args)
-{
-  flightgear::Waypt* w = wayptGhost(me);
-  if (!w) {
-    naRuntimeError(c, "waypoint.navaid called on non-waypoint object");
-  }
-  
-  FGPositioned* pos = w->source();
-  if (!pos) {
-    return naNil();
-  }
-  
-  switch (pos->type()) {
-  case FGPositioned::VOR:
-  case FGPositioned::NDB:
-  case FGPositioned::ILS:
-  case FGPositioned::LOC:
-  case FGPositioned::GS:
-  case FGPositioned::DME:
-  case FGPositioned::TACAN: {
-    FGNavRecord* nav = (FGNavRecord*) pos;
-    return ghostForNavaid(c, nav);
-  }
-      
-  default:
-    return naNil();
-  }
-}
-
-static naRef f_waypoint_airport(naContext c, naRef me, int argc, naRef* args)
-{
-  flightgear::Waypt* w = wayptGhost(me);
-  if (!w) {
-    naRuntimeError(c, "waypoint.navaid called on non-waypoint object");
-  }
-  
-  FGPositioned* pos = w->source();
-  if (!pos || FGAirport::isAirportType(pos)) {
-    return naNil();
-  }
-  
-  return ghostForAirport(c, (FGAirport*) pos);
-}
-
-static naRef f_waypoint_runway(naContext c, naRef me, int argc, naRef* args)
-{
-  flightgear::Waypt* w = wayptGhost(me);
-  if (!w) {
-    naRuntimeError(c, "waypoint.navaid called on non-waypoint object");
-  }
-  
-  FGPositioned* pos = w->source();
-  if (!pos || (pos->type() != FGPositioned::RUNWAY)) {
-    return naNil();
-  }
-  
-  return ghostForRunway(c, (FGRunway*) pos);
-}
-
 static naRef f_procedure_transition(naContext c, naRef me, int argc, naRef* args)
 {
   Procedure* proc = procedureGhost(me);
@@ -2840,11 +2826,7 @@ naRef initNasalPositioned(naRef globals, naContext c)
   
     waypointPrototype = naNewHash(c);
     naSave(c, waypointPrototype);
-    
-    hashset(c, waypointPrototype, "navaid", naNewFunc(c, naNewCCode(c, f_waypoint_navaid)));
-    hashset(c, waypointPrototype, "runway", naNewFunc(c, naNewCCode(c, f_waypoint_runway)));
-    hashset(c, waypointPrototype, "airport", naNewFunc(c, naNewCCode(c, f_waypoint_airport)));
-  
+
     procedurePrototype = naNewHash(c);
     naSave(c, procedurePrototype);
     hashset(c, procedurePrototype, "transition", naNewFunc(c, naNewCCode(c, f_procedure_transition)));

commit c28f63132f3ed6c24593d8822d77fac0329e729a

Re-add airport(), navaid() and runway() methods of flightplan-leg ghosts
These methods were removed by mistake in commit 0dbb0dff9, which broke
code that relies on them (e.g., FMSDelegate.currentWaypointChanged() in
$FG_ROOT/Nasal/route_manager.nas).

Also remove 'waypointPrototype' from NasalPositioned.cxx, since it is
not used anymore.

Note: the methods can't be easily re-enabled by means of
      'waypointPrototype', because for FPLeg ghosts, 'waypointPrototype'
      was emulated as a parent class before this commit, and thus when
      querying an FPLeg ghost for its 'airport', 'runway' or 'navaid'
      member, the corresponding data member returned by
      waypointCommonGetMember() would be found by legGhostGetMember()
      *before* parent classes are searched. This commit prevents this
      from happening by returning 'airport', 'runway' and 'navaid' as
      member functions *directly* in legGhostGetMember(), before
      waypointCommonGetMember() is queried as a fallback.

Thanks to Eric van den Berg for the bug report.

src/Scripting/NasalPositioned.cxx
@@ -269,7 +269,6 @@ static void routeBaseGhostDestroy(void* g)
 
 static naRef airportPrototype;
 static naRef flightplanPrototype;
-static naRef waypointPrototype;
 static naRef geoCoordClass;
 static naRef fpLegPrototype;
 static naRef procedurePrototype;
@@ -558,6 +557,45 @@ naRef routeRestrictionToNasal(naContext c, RouteRestriction rr)
   return naNil();
 }
 
+// navaid() method of FPLeg ghosts
+static naRef f_fpLeg_navaid(naContext c, naRef me, int argc, naRef* args)
+{
+  flightgear::Waypt* w = wayptGhost(me);
+  if (!w) {
+    naRuntimeError(c,
+                   "flightplan-leg.navaid() called, but can't find the "
+                   "underlying waypoint for the flightplan-leg object");
+  }
+
+  return waypointNavaid(c, w);
+}
+
+// airport() method of FPLeg ghosts
+static naRef f_fpLeg_airport(naContext c, naRef me, int argc, naRef* args)
+{
+  flightgear::Waypt* w = wayptGhost(me);
+  if (!w) {
+    naRuntimeError(c,
+                   "flightplan-leg.airport() called, but can't find the "
+                   "underlying waypoint for the flightplan-leg object");
+  }
+
+  return waypointAirport(c, w);
+}
+
+// runway() method of FPLeg ghosts
+static naRef f_fpLeg_runway(naContext c, naRef me, int argc, naRef* args)
+{
+  flightgear::Waypt* w = wayptGhost(me);
+  if (!w) {
+    naRuntimeError(c,
+                   "flightplan-leg.runway() called, but can't find the "
+                   "underlying waypoint for the flightplan-leg object");
+  }
+
+  return waypointRunway(c, w);
+}
+
 static const char* legGhostGetMember(naContext c, void* g, naRef field, naRef* out)
 {
   const char* fieldName = naStr_data(field);
@@ -567,7 +605,6 @@ static const char* legGhostGetMember(naContext c, void* g, naRef field, naRef* o
   if (!strcmp(fieldName, "parents")) {
     *out = naNewVector(c);
     naVec_append(*out, fpLegPrototype);
-    naVec_append(*out, waypointPrototype);
   } else if (!strcmp(fieldName, "index")) {
     *out = naNum(leg->index());
   } else if (!strcmp(fieldName, "alt_cstr")) {
@@ -585,6 +622,12 @@ static const char* legGhostGetMember(naContext c, void* g, naRef field, naRef* o
     *out = naNum(leg->courseDeg());
   } else if (!strcmp(fieldName, "distance_along_route")) {
     *out = naNum(leg->distanceAlongRoute());
+  } else if (!strcmp(fieldName, "airport")) {
+    *out = naNewFunc(c, naNewCCode(c, f_fpLeg_airport));
+  } else if (!strcmp(fieldName, "navaid")) {
+    *out = naNewFunc(c, naNewCCode(c, f_fpLeg_navaid));
+  } else if (!strcmp(fieldName, "runway")) {
+    *out = naNewFunc(c, naNewCCode(c, f_fpLeg_runway));
   } else { // check for fields defined on the underlying waypoint
     return waypointCommonGetMember(c, wpt, fieldName, out);
   }
@@ -2822,10 +2865,6 @@ naRef initNasalPositioned(naRef globals, naContext c)
     hashset(c, flightplanPrototype, "indexOfWP", naNewFunc(c, naNewCCode(c, f_flightplan_indexOfWp)));
 
     hashset(c, flightplanPrototype, "save", naNewFunc(c, naNewCCode(c, f_flightplan_save)));
-
-  
-    waypointPrototype = naNewHash(c);
-    naSave(c, waypointPrototype);
 
     procedurePrototype = naNewHash(c);
     naSave(c, procedurePrototype);