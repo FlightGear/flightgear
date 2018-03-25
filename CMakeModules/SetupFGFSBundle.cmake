e88017eba35b070199c09c0a63024985c5c6102c
Edward d'Auvergne <edward@nmr-relax.com>
2018-03-25 18:51:10 +0200

    CMake: Shifted the Mac bundle commands into its own CMake module.

---
+++ CMakeModules/SetupFGFSBundle.cmake
@@ -0,0 +1,19 @@
+function(setup_fgfs_bundle target)
+    execute_process(COMMAND date +%Y
+        OUTPUT_VARIABLE CURRENT_YEAR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    # in our local CMakeModules dir
+    set_target_properties(${target} PROPERTIES
+        MACOSX_BUNDLE_INFO_PLIST FlightGearBundleInfo.plist.in
+        MACOSX_BUNDLE_GUI_IDENTIFIER "org.flightgear.FlightGear"
+        MACOSX_BUNDLE_SHORT_VERSION_STRING ${FLIGHTGEAR_VERSION}
+        MACOSX_BUNDLE_LONG_VERSION_STRING "FlightGear ${FLIGHTGEAR_VERSION} Nightly"
+        MACOSX_BUNDLE_BUNDLE_VERSION ${FLIGHTGEAR_VERSION}
+        MACOSX_BUNDLE_COPYRIGHT "FlightGear ${FLIGHTGEAR_VERSION} © 1997-${CURRENT_YEAR}, The FlightGear Project. Licensed under the GNU Public License version 2."
+        MACOSX_BUNDLE_INFO_STRING "Nightly build of FlightGear ${FLIGHTGEAR_VERSION} for testing and development"
+        MACOSX_BUNDLE_BUNDLE_NAME "FlightGear"
+        MACOSX_BUNDLE_ICON_FILE "FlightGear.icns"
+    )
+endfunction()
