94f82c47ef5333d66b306357839eb313130c16fd
ThomasS <thomass@yard.de>
2018-04-24 06:20:28 +0200

    Additional departure airport/time and arrival airport/time information in the property tree for AI aircraft.

--- src/Traffic/Schedule.cxx
+++ src/Traffic/Schedule.cxx
@@ -383,6 +383,17 @@ bool FGAISchedule::createAIAircraft(FGScheduledFlight* flight, double speedKnots
   if (fp->isValidPlan()) {
         aiAircraft->FGAIBase::setFlightPlan(std::move(fp));
         globals->get_subsystem<FGAIManager>()->attach(aiAircraft);
+        if (aiAircraft->_getProps()) {
+            SGPropertyNode *nodeofaircraft = aiAircraft->_getProps();
+            if (dep) {
+                nodeofaircraft->getChild("departureairport", 0, true)->setStringValue(dep->getId());
+                nodeofaircraft->getChild("departuretime", 0, true)->setIntValue(deptime);
+            }
+            if (arr) {
+                nodeofaircraft->getChild("arrivalairport", 0, true)->setStringValue(arr->getId());                        
+		// arrival time not known here
+            }
+        }
         return true;
   } else {
         aiAircraft = NULL;
