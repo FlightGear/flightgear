f8c2cb52840bcf0559b8aa027669913259cf8e0b
Edward d'Auvergne <edward@nmr-relax.com>
2018-06-14 21:19:00 +0200

    TestSuite: Migration of the old Navaids tests to the CppUnit framework.
    This test suffers from significant bit-rot (it is designed for operation prior
    to the NavDataCache), so it is deactivated.

--- test_suite/system_tests/Navaids/testnavs.cxx
+++ test_suite/system_tests/Navaids/testnavs.cxx
@@ -16,33 +16,40 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+#include <iostream>
+
 #include <simgear/misc/sg_path.hxx>
 
-#include "fixlist.hxx"
-#include "ilslist.hxx"
-#include "navlist.hxx"
-#include "mkrbeacons.hxx"
+#include <Navaids/fixlist.hxx>
+//#include "ilslist.hxx"
+#include <Navaids/navlist.hxx>
+//#include "mkrbeacons.hxx"
 
-// change this!
-const string FG_DATA_DIR("/usr/local/lib/FlightGear");
+#include "test_suite/dataStore.hxx"
 
-int main() {
+#include "testnavs.hxx"
+
+
+void NavaidsTests::testNavs()
+{
     double heading, dist;
 
+    DataStore &data = DataStore::get();
+
     FGNavList *current_navlist = new FGNavList;
-    SGPath p_nav( FG_DATA_DIR + "/Navaids/default.nav" );
+    SGPath p_nav = data.getFGRoot() / "/Navaids/default.nav";
 
     current_navlist->init( p_nav );
-	
+
     FGNavRecord *n;
     if ( (n = current_navlist->findByFreq( -93.2 * SG_DEGREES_TO_RADIANS,
                                            45.14 * SG_DEGREES_TO_RADIANS,
                                            3000, 117.30)) != NULL )
     {
-	cout << "Found a vor station in range" << endl;
-	cout << " id = " << n->get_ident() << endl;
+        std::cout << "Found a vor station in range" << std::endl;
+        std::cout << " id = " << n->get_ident() << std::endl;
     } else {
-	cout << "not picking up vor. :-(" << endl;
+        std::cout << "not picking up vor. :-(" << std::endl;
     }
 
     FGNavRecord *dcs;
@@ -50,35 +57,35 @@ int main() {
                                               -3.3 * SG_DEGREES_TO_RADIANS,
                                               55.9 * SG_DEGREES_TO_RADIANS))
          != NULL ) {
-						
-        cout << "Found DCS by ident" << endl;
+
+        std::cout << "Found DCS by ident" << std::endl;
         if (dcs->get_freq() != 11520)
-            cout << "Frequency for DCS VOR is wrong (should be 115.20), it's " 
-                 << dcs->get_freq() << endl;
+            std::cout << "Frequency for DCS VOR is wrong (should be 115.20), it's " 
+                 << dcs->get_freq() << std::endl;
     } else {
-        cout << "couldn't locate DCS (Dean-Cross) VOR" << endl;
+        std::cout << "couldn't locate DCS (Dean-Cross) VOR" << std::endl;
     }
-	
+
     // we have to init the marker beacon storage before we parse the ILS file
     FGMarkerBeacons *current_beacons = new FGMarkerBeacons;
     current_beacons->init();
 	
     FGILSList *current_ilslist = new FGILSList;
-    SGPath p_ils( FG_DATA_DIR + "/Navaids/default.ils" );
+    SGPath p_ils = data.getFGRoot() / "/Navaids/default.ils";
     current_ilslist->init( p_ils );
     FGILS *i = current_ilslist->findByFreq( -93.1 * SG_DEGREES_TO_RADIANS,
                                             45.24 * SG_DEGREES_TO_RADIANS,
                                             3000, 110.30);
     if ( i != NULL ) {
-	cout << "Found an ils station in range" << endl;
-	cout << " apt = " << i->get_aptcode() << endl;
-	cout << " rwy = " << i->get_rwyno() << endl;
+        std::cout << "Found an ils station in range" << std::endl;
+        std::cout << " apt = " << i->get_aptcode() << std::endl;
+        std::cout << " rwy = " << i->get_rwyno() << std::endl;
     } else {
-	cout << "not picking up ils. :-(" << endl;
+        std::cout << "not picking up ils. :-(" << std::endl;
     }
 
     FGFixList *current_fixlist = new FGFixList;
-    SGPath p_fix( FG_DATA_DIR + "/Navaids/default.fix" );
+    SGPath p_fix = data.getFGRoot() / "/Navaids/default.fix";
     current_fixlist->init( p_fix );
     FGFix fix;
 	
@@ -90,11 +97,11 @@ int main() {
                                             53.698 * SG_DEGREES_TO_RADIANS,
                                             3000, &fix, &heading, &dist) )
     {
-	cout << "Found a matching fix" << endl;
-	cout << " id = " << fix.get_ident() << endl;
-	cout << " heading = " << heading << " dist = " << dist * SG_METER_TO_NM
-             << endl;
+        std::cout << "Found a matching fix" << std::endl;
+        std::cout << " id = " << fix.get_ident() << std::endl;
+        std::cout << " heading = " << heading << " dist = " << dist * SG_METER_TO_NM
+             << std::endl;
     } else {
-	cout << "did not find fix. :-(" << endl;
+        CPPUNIT_FAIL("did not find fix. :-(");
     }
 }
