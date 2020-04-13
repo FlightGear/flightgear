7e985f0a8a601c069aac90894edcd4ad29fa8e09
James Turner <zakalawe@mac.com>
2020-04-13 09:35:52 +0100

    Launcher: allow ‘Airports’ when adding a scenery path
    Adjust the warning message to allow for ‘Airports-only’ scenery paths
    to be added.

--- src/GUI/AddOnsController.cxx
+++ src/GUI/AddOnsController.cxx
@@ -100,7 +100,7 @@ QString AddOnsController::addSceneryPath() const
     SGPath p(path.toStdString());
     bool isValid = false;
 
-    for (const auto& dir: {"Objects", "Terrain", "Buildings", "Roads", "Pylons", "NavData"}) {
+    for (const auto& dir: {"Objects", "Terrain", "Buildings", "Roads", "Pylons", "NavData", "Airports"}) {
         if ((p / dir).exists()) {
             isValid = true;
             break;
@@ -113,7 +113,7 @@ QString AddOnsController::addSceneryPath() const
         mb.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
         mb.setDefaultButton(QMessageBox::No);
         mb.setInformativeText(tr("Added scenery should contain at least one of the following "
-                                 "folders: Objects, Terrain, Buildings, Roads, Pylons, NavData."));
+                                 "folders: Objects, Terrain, Buildings, Roads, Pylons, NavData, Airports."));
         mb.exec();
 
         if (mb.result() == QMessageBox::No) {
