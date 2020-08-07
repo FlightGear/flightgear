75e28368eabb24b10eb6d0d1985944d525a8026a
Scott Giese <scttgs0@gmail.com>
2020-08-06 22:01:15 -0500

    Memory Leak: 57 MB
    60,200,752 (59,468,656 direct, 732,096 indirect) bytes in 285,907 blocks are definitely lost in loss record 15,499 of 15,501

--- src/Traffic/TrafficMgr.cxx
+++ src/Traffic/TrafficMgr.cxx
@@ -493,6 +493,12 @@ void FGTrafficManager::shutdown()
         cachefile.close();
     }
     scheduledAircraft.clear();
+
+    // REVIEW: Memory Leak
+    //       60,200,752 (59,468,656 direct, 732,096 indirect) bytes in 285,907 blocks are definitely lost in loss record 15,499 of 15,501
+    // for (auto flight : flights[requiredAircraft]) {
+    //     delete flight;
+    // }
     flights.clear();
 
     currAircraft = scheduledAircraft.begin();
