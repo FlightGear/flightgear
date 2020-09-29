329d3cdd13f58cd4c1ac6860fda30c623b68cd10
James Turner <zakalawe@mac.com>
2020-09-29 12:30:28 +0100

    PLIB includes fix from Florent

--- CMakeModules/SetupFGFSIncludes.cmake
+++ CMakeModules/SetupFGFSIncludes.cmake
@@ -4,8 +4,8 @@ function(setup_fgfs_includes target)
         target_include_directories(${target} PRIVATE ${PROJECT_SOURCE_DIR}/src/FDM/JSBSim)
     endif()
 
+    target_include_directories(${target} PRIVATE ${PLIB_INCLUDE_DIR})
     target_include_directories(${target} PRIVATE ${PROJECT_SOURCE_DIR}/3rdparty/cjson)
-
     # only actually needed for httpd.cxx
     target_include_directories(${target} PRIVATE ${PROJECT_SOURCE_DIR}/3rdparty/mongoose)
 
