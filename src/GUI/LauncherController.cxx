9855e89109d8c44dd9a9c967dc187c8c4e4f3650
Automatic Release Builder <build@flightgear.org>
2020-10-27 19:24:13 +0000

    Launcher: Validate restored aircraft state on load
    Otherwise a stray state can enter the logic, unless the user
    re-selects it.

--- src/GUI/LauncherController.cxx
+++ src/GUI/LauncherController.cxx
@@ -457,6 +457,13 @@ void LauncherController::updateSelectedAircraft()
         }
     }
 
+    if (!m_aircraftState.isEmpty()) {
+      if (!m_selectedAircraftInfo->hasState(m_aircraftState)) {
+        m_aircraftState.clear();
+        emit selectedAircraftStateChanged();
+      }
+    }
+
     emit canFlyChanged();
 }
 
