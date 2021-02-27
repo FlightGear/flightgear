380e0c1105b6f22281a8ab61b69c58d4b1c69fb8
Scott Giese <scttgs0@gmail.com>
2021-02-26 21:13:13 -0600

    Maintenance: ExternalNet
    Prevent buffer overflow.
    Inconsistent whitespace use.

--- src/FDM/ExternalNet/ExternalNet.cxx
+++ src/FDM/ExternalNet/ExternalNet.cxx
@@ -21,7 +21,7 @@
 // $Id$
 
 #ifdef HAVE_CONFIG_H
-#  include <config.h>
+#include <config.h>
 #endif
 
 #include <cstring>
@@ -39,29 +39,27 @@
 
 class HTTPClient : public simgear::NetBufferChannel
 {
-
     bool done;
     SGTimeStamp start;
     simgear::NetChannelPoller poller;
+
 public:
-
-    HTTPClient ( const char* host, int port, const char* path ) :
-        done( false )
+    HTTPClient(const char* host, int port, const char* path) : done(false)
     {
-        open ();
-        connect (host, port);
+        open();
+        connect(host, port);
 
-        char buffer[256];
-        ::snprintf (buffer, 255, "GET %s HTTP/1.0\r\n\r\n", path);
-        buffer[255] = '\0';
+        char buffer[300];
+        ::snprintf(buffer, 299, "GET %s HTTP/1.0\r\n\r\n", path);
+        buffer[299] = '\0';
 
-        bufferSend(buffer, strlen(buffer) ) ;
+        bufferSend(buffer, strlen(buffer));
 
         poller.addChannel(this);
         start.stamp();
     }
 
-    virtual void handleBufferRead (simgear::NetBuffer& buffer)
+    virtual void handleBufferRead(simgear::NetBuffer& buffer)
     {
         const char* s = buffer.getData();
         while (*s)
@@ -75,23 +73,24 @@ public:
     }
 
     bool isDone() const { return done; }
-    bool isDone( long usec ) const { 
-        if ( start + SGTimeStamp::fromUSec(usec) < SGTimeStamp::now() ) {
+    bool isDone(long usec) const
+    {
+        if (start + SGTimeStamp::fromUSec(usec) < SGTimeStamp::now()) {
             return true;
         } else {
             return done;
         }
     }
-    
+
     void poll(int timeout)
     {
         poller.poll(timeout);
     }
 };
 
-FGExternalNet::FGExternalNet( double dt, string host, int dop, int dip, int cp )
+FGExternalNet::FGExternalNet(double dt, string host, int dop, int dip, int cp)
 {
-//     set_delta_t( dt );
+    //     set_delta_t( dt );
 
     valid = true;
 
@@ -103,43 +102,44 @@ FGExternalNet::FGExternalNet( double dt, string host, int dop, int dip, int cp )
     /////////////////////////////////////////////////////////
     // Setup client udp connection (sends data to remote fdm)
 
-    if ( ! data_client.open( false ) ) {
-	SG_LOG( SG_FLIGHT, SG_ALERT, "Error opening client data channel" );
-	valid = false;
+    if (!data_client.open(false)) {
+        SG_LOG(SG_FLIGHT, SG_ALERT, "Error opening client data channel");
+        valid = false;
     }
 
     // fire and forget
-    data_client.setBlocking( false );
+    data_client.setBlocking(false);
 
-    if ( data_client.connect( fdm_host.c_str(), data_out_port ) == -1 ) {
+    if (data_client.connect(fdm_host.c_str(), data_out_port) == -1) {
         printf("error connecting to %s:%d\n", fdm_host.c_str(), data_out_port);
-	valid = false;
+        valid = false;
     }
 
     /////////////////////////////////////////////////////////
     // Setup server udp connection (for receiving data)
 
-    if ( ! data_server.open( false ) ) {
-	SG_LOG( SG_FLIGHT, SG_ALERT, "Error opening client server channel" );
-	valid = false;
+    if (!data_server.open(false)) {
+        SG_LOG(SG_FLIGHT, SG_ALERT, "Error opening client server channel");
+        valid = false;
     }
 
     // disable blocking
-    data_server.setBlocking( false );
+    data_server.setBlocking(false);
 
     // allowed to read from a broadcast addr
     // data_server.setBroadcast( true );
 
     // if we bind to fdm_host = "" then we accept messages from
     // anyone.
-    if ( data_server.bind( "", data_in_port ) == -1 ) {
+    if (data_server.bind("", data_in_port) == -1) {
         printf("error binding to port %d\n", data_in_port);
-	valid = false;
+        valid = false;
     }
 }
 
 
-FGExternalNet::~FGExternalNet() {
+FGExternalNet::~FGExternalNet()
+{
     data_client.close();
     data_server.close();
 }
@@ -147,93 +147,94 @@ FGExternalNet::~FGExternalNet() {
 
 // Initialize the ExternalNet flight model, dt is the time increment
 // for each subsequent iteration through the EOM
-void FGExternalNet::init() {
+void FGExternalNet::init()
+{
     // cout << "FGExternalNet::init()" << endl;
 
     // Explicitly call the superclass's
     // init method first.
     common_init();
 
-    double lon = fgGetDouble( "/sim/presets/longitude-deg" );
-    double lat = fgGetDouble( "/sim/presets/latitude-deg" );
-    double alt = fgGetDouble( "/sim/presets/altitude-ft" );
+    double lon = fgGetDouble("/sim/presets/longitude-deg");
+    double lat = fgGetDouble("/sim/presets/latitude-deg");
+    double alt = fgGetDouble("/sim/presets/altitude-ft");
     double ground = get_Runway_altitude_m();
     double heading = fgGetDouble("/sim/presets/heading-deg");
-    double speed = fgGetDouble( "/sim/presets/airspeed-kt" );
+    double speed = fgGetDouble("/sim/presets/airspeed-kt");
 
     char cmd[256];
 
-    HTTPClient *http;
-    sprintf( cmd, "/longitude-deg?value=%.8f", lon );
-    http = new HTTPClient( fdm_host.c_str(), cmd_port, cmd );
-    while ( !http->isDone(1000000) ) http->poll(0);
+    HTTPClient* http;
+    sprintf(cmd, "/longitude-deg?value=%.8f", lon);
+    http = new HTTPClient(fdm_host.c_str(), cmd_port, cmd);
+    while (!http->isDone(1000000)) http->poll(0);
     delete http;
 
-    sprintf( cmd, "/latitude-deg?value=%.8f", lat );
-    http = new HTTPClient( fdm_host.c_str(), cmd_port, cmd );
-    while ( !http->isDone(1000000) ) http->poll(0);
+    sprintf(cmd, "/latitude-deg?value=%.8f", lat);
+    http = new HTTPClient(fdm_host.c_str(), cmd_port, cmd);
+    while (!http->isDone(1000000)) http->poll(0);
     delete http;
 
-    sprintf( cmd, "/altitude-ft?value=%.8f", alt );
-    http = new HTTPClient( fdm_host.c_str(), cmd_port, cmd );
-    while ( !http->isDone(1000000) ) http->poll(0);
+    sprintf(cmd, "/altitude-ft?value=%.8f", alt);
+    http = new HTTPClient(fdm_host.c_str(), cmd_port, cmd);
+    while (!http->isDone(1000000)) http->poll(0);
     delete http;
 
-    sprintf( cmd, "/ground-m?value=%.8f", ground );
-    http = new HTTPClient( fdm_host.c_str(), cmd_port, cmd );
-    while ( !http->isDone(1000000) ) http->poll(0);
+    sprintf(cmd, "/ground-m?value=%.8f", ground);
+    http = new HTTPClient(fdm_host.c_str(), cmd_port, cmd);
+    while (!http->isDone(1000000)) http->poll(0);
     delete http;
 
-    sprintf( cmd, "/speed-kts?value=%.8f", speed );
-    http = new HTTPClient( fdm_host.c_str(), cmd_port, cmd );
-    while ( !http->isDone(1000000) ) http->poll(0);
+    sprintf(cmd, "/speed-kts?value=%.8f", speed);
+    http = new HTTPClient(fdm_host.c_str(), cmd_port, cmd);
+    while (!http->isDone(1000000)) http->poll(0);
     delete http;
 
-    sprintf( cmd, "/heading-deg?value=%.8f", heading );
-    http = new HTTPClient( fdm_host.c_str(), cmd_port, cmd );
-    while ( !http->isDone(1000000) ) http->poll(0);
+    sprintf(cmd, "/heading-deg?value=%.8f", heading);
+    http = new HTTPClient(fdm_host.c_str(), cmd_port, cmd);
+    while (!http->isDone(1000000)) http->poll(0);
     delete http;
 
-    SG_LOG( SG_IO, SG_INFO, "before sending reset command." );
+    SG_LOG(SG_IO, SG_INFO, "before sending reset command.");
 
-    if( fgGetBool("/sim/presets/onground") ) {
-      sprintf( cmd, "/reset?value=ground" );
+    if (fgGetBool("/sim/presets/onground")) {
+        sprintf(cmd, "/reset?value=ground");
     } else {
-      sprintf( cmd, "/reset?value=air" );
+        sprintf(cmd, "/reset?value=air");
     }
-    http = new HTTPClient( fdm_host.c_str(), cmd_port, cmd );
-    while ( !http->isDone(1000000) ) http->poll(0);
+    http = new HTTPClient(fdm_host.c_str(), cmd_port, cmd);
+    while (!http->isDone(1000000)) http->poll(0);
     delete http;
 
-    SG_LOG( SG_IO, SG_INFO, "Remote FDM init() finished." );
+    SG_LOG(SG_IO, SG_INFO, "Remote FDM init() finished.");
 }
 
 
 // Run an iteration of the EOM.
-void FGExternalNet::update( double dt ) {
+void FGExternalNet::update(double dt)
+{
     int length;
     int result;
 
     if (is_suspended())
-      return;
+        return;
 
     // Send control positions to remote fdm
     length = sizeof(ctrls);
-    FGProps2NetCtrls( &ctrls, true, true );
-    if ( data_client.send( (char *)(& ctrls), length, 0 ) != length ) {
-	SG_LOG( SG_IO, SG_DEBUG, "Error writing data." );
+    FGProps2NetCtrls(&ctrls, true, true);
+    if (data_client.send((char*)(&ctrls), length, 0) != length) {
+        SG_LOG(SG_IO, SG_DEBUG, "Error writing data.");
     } else {
-	SG_LOG( SG_IO, SG_DEBUG, "wrote control data." );
+        SG_LOG(SG_IO, SG_DEBUG, "wrote control data.");
     }
 
     // Read next set of FDM data (blocking enabled to maintain 'sync')
     length = sizeof(fdm);
-    while ( (result = data_server.recv( (char *)(& fdm), length, 0)) >= 0 ) {
-	SG_LOG( SG_IO, SG_DEBUG, "Success reading data." );
-	FGNetFDM2Props( &fdm );
+    while ((result = data_server.recv((char*)(&fdm), length, 0)) >= 0) {
+        SG_LOG(SG_IO, SG_DEBUG, "Success reading data.");
+        FGNetFDM2Props(&fdm);
     }
 }
-
 
 // Register the subsystem.
 #if 0
