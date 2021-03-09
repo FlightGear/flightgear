6e42c8fc5716839bf246bf12734d251281d621ba
James Turner <james@flightgear.org>
2021-03-09 10:46:46 +0000

    Restore compatibility with CMake < 3.12

--- CMakeModules/SetupFGFSLibraries.cmake
+++ CMakeModules/SetupFGFSLibraries.cmake
@@ -67,3 +67,78 @@ function(setup_fgfs_libraries target)
         target_link_libraries(${target} sentry::sentry)
     endif()
 endfunction()
+
+
+# CMake < 3.12 can't define a link to an OBJECT library to specify its include
+# paths, so we have to essentially duplicate the above and configure the paths manually.
+# Once we require CMake 3.12, delete all this and use the function above.
+
+function (_apply_target_includes dest target)
+    if (NOT TARGET ${target})
+        return()
+    endif()
+
+    # retrieve the list of includes from the target
+    get_target_property(includes ${target} INTERFACE_INCLUDE_DIRECTORIES)
+
+    foreach(i ${includes})
+        # skip any invalid includes
+        if (NOT i)
+            return()
+        endif()
+
+        target_include_directories(${dest} PUBLIC ${i})
+    endforeach()
+endfunction()
+
+function (_apply_all_target_includes dest)
+    foreach(arg IN LISTS ARGN)
+      _apply_target_includes(${dest} ${arg})
+    endforeach()
+endfunction()
+
+function (setup_fgfs_library_includes target)
+ 
+    _apply_all_target_includes(${target} 
+        SimGearCore
+        SimGearScene
+        Boost::boost
+        ${EVENT_INPUT_LIBRARIES}
+        ${HLA_LIBRARIES}
+        ${OPENGL_LIBRARIES}
+        ${OPENSCENEGRAPH_LIBRARIES}
+        ${PLATFORM_LIBS}
+        ${PLIB_LIBRARIES}
+    )
+
+    if(ENABLE_JSBSIM)
+        _apply_target_includes(${target} JSBSim)
+    endif()    
+    
+    _apply_target_includes(${target} fgsqlite3)
+    _apply_target_includes(${target} fgvoicesynth)
+    _apply_target_includes(${target} fgembeddedresources)
+
+    if (HAVE_QT)
+        _apply_all_target_includes(${target} Qt5::Core Qt5::Widgets Qt5::Gui)
+    endif()
+
+    if(ENABLE_IAX)
+        _apply_target_includes(${target} iaxclient_lib)
+    endif()
+
+    if (ENABLE_SWIFT)
+        _apply_all_target_includes(${target} ${dbus_target})
+    endif()
+
+    if (ENABLE_PLIB_JOYSTICK)
+        _apply_target_includes(${target} PLIBJoystick)
+    endif()
+    
+    _apply_target_includes(${target} PLIBFont)
+
+    if (TARGET sentry::sentry)
+        _apply_target_includes(${target} sentry::sentry)
+    endif()
+
+endfunction()
