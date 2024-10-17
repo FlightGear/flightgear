d1377c6888ecf510dd3998bb61603ff25e2a12e7
James Turner <zakalawe@mac.com>
2024-10-17 09:44:53 +0200

    CI: make symbol upload target condtional
    - never upload symbols in Dev or Nightly builds

--- CMakeModules/ExportDebugSymbols.cmake
+++ CMakeModules/ExportDebugSymbols.cmake
@@ -7,7 +7,6 @@ add_custom_target(
 
 function(export_debug_symbols target)
 
-
     if (APPLE)
         # workaround because we adjust the bundle name in some cases, and
         # TARGET_FILE seems to have a bug of assuming the executable name matches
@@ -44,11 +43,18 @@ endfunction()
 file(TO_NATIVE_PATH "${FINAL_MSVC_3RDPARTY_DIR}/bin" _msvc_3rdparty_bin_dir)
 set(CMAKE_MSVCIDE_RUN_PATH "${_msvc_3rdparty_bin_dir}")
 
-add_custom_target(upload_debug_symbols
-    COMMENT "Uploading debug symbols via sentry-cli"
-    COMMAND sentry-cli upload-dif --org flightgear --project flightgear ${CMAKE_BINARY_DIR}/symbols
-) 
+if (FG_BUILD_TYPE STREQUAL "Release")
+    add_custom_target(upload_debug_symbols
+        COMMENT "Uploading debug symbols via sentry-cli"
+        COMMAND sentry-cli upload-dif --org flightgear --project flightgear ${CMAKE_BINARY_DIR}/symbols
+    ) 
 
+    add_dependencies(upload_debug_symbols debug_symbols)
+else()
+    # make a dummy target to keep CI simple, but it doesn't do anything
+    add_custom_target(upload_debug_symbols
+        COMMENT "Non-release FG_BUILD_TYPE: symbol upload is disabled"
+    ) 
+endif()
 
-add_dependencies(upload_debug_symbols debug_symbols)
 
