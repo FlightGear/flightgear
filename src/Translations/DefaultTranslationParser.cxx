67a89c8ad312b14e466882423e6fadd803cfa533
Florent Rougon <f.rougon@frougon.net>
2025-02-25 09:40:33 +0100

    DefaultTranslationParser: handle and require the new format
    The new format has a header like this, so we can easily change things if
    needed in the future:
    
      <meta>
        <file-type>FlightGear default translation file</file-type>
        <format-version>1</format-version>
        <description>...</description>
        <language-description>Engineering English</language-description>
      </meta>

--- src/Translations/DefaultTranslationParser.cxx
+++ src/Translations/DefaultTranslationParser.cxx
@@ -6,6 +6,7 @@
 
 #include <cassert>
 #include <string>
+#include <utility>
 
 #include <simgear/structure/exception.hxx>
 
@@ -38,42 +39,163 @@ bool DefaultTranslationParser::asBoolean(const string& str)
     throw sg_io_exception(message, location, SG_ORIGIN, false);
 }
 
+[[noreturn]] void DefaultTranslationParser::parseError(const std::string& message)
+{
+    const sg_location location(getPath(), getLine(), getColumn());
+    throw sg_io_exception(message, location, SG_ORIGIN, false);
+}
+
 void DefaultTranslationParser::startElement(const char* name,
                                             const XMLAttributes& attrs)
 {
-    _nestingLevel++;
-
-    if (_nestingLevel == 1) {   // we are processing the root element
-        return;
-    } else if (_nestingLevel > 2) {
-        const string message = "nesting elements is not supported";
-        const sg_location location(getPath(), getLine(), getColumn());
-        throw sg_io_exception(message, location, SG_ORIGIN, false);
-    }
-
-    _name = name;
+    bool allowedElement;
     const char* hasPluralStr = attrs.getValue("has-plural");
-    _hasPlural = hasPluralStr && asBoolean(hasPluralStr);
-    _sourceText.clear();
+
+    switch (_state) {
+    case State::LOOKING_FOR_RESOURCE_ELEMENT:
+        if (!std::strcmp(name, "resource")) {
+            _state = State::LOOKING_FOR_META_ELEMENT;
+        } else {
+            parseError("Expected the root element to be 'resource', "
+                       "but found '" + string(name) + "' instead");
+        }
+        break;
+    case State::LOOKING_FOR_META_ELEMENT:
+        if (!std::strcmp(name, "meta")) {
+            _state = State::READING_META_ELEMENT;
+        } else {
+            parseError("Expected a 'meta' element here, but found '"
+                       + string(name) + "' instead");
+        }
+        break;
+    case State::READING_META_ELEMENT:
+        allowedElement = false;
+
+        for (const string eltName : {"file-type", "format-version", "description",
+                "language-description"}) {
+            if (name == eltName) {
+                startElementInsideMeta(eltName);
+                allowedElement = true;
+                break;
+            }
+        }
+
+        if (!allowedElement) {
+            parseError("Unexpected element '" + string(name) +
+                       "' inside 'meta' element");
+        }
+        break;
+    case State::LOOKING_FOR_STRINGS_ELEMENT:
+        if (!std::strcmp(name, "strings")) {
+            _state = State::READING_STRINGS_ELEMENT;
+        } else {
+            parseError("Expected a 'strings' element after 'meta', but found '"
+                       + string(name) + "'");
+        }
+        break;
+    case State::READING_STRINGS_ELEMENT:
+        _stringTagName = name;
+        _hasPlural = hasPluralStr && asBoolean(hasPluralStr);
+        _text.clear(); // we'll gather the element's contents
+        _state = State::READING_TRANSLATABLE_STRING;
+        break;
+    case State::READING_FILE_TYPE_ELEMENT:
+        parseError("Unexpected element '" + string(name) +
+                   "' inside <file-type>");
+    case State::READING_FORMAT_VERSION_ELEMENT:
+        parseError("Unexpected element '" + string(name) +
+                   "' inside <format-version>");
+    case State::READING_TRANSLATABLE_STRING:
+        parseError("Unexpected element '" + string(name) +
+                   "' inside translatable string '" + _stringTagName + "'");
+    case State::AFTER_STRINGS_ELEMENT:
+        parseError("Unexpected element '" + string(name) +
+                   "' after the 'strings' element");
+    }
+}
+
+void DefaultTranslationParser::startElementInsideMeta(const std::string& name)
+{
+    if (name == "file-type") {
+        if (_foundFileType) {
+            parseError("Only one 'file-type' element is allowed inside 'meta'.");
+        }
+
+        _foundFileType = true;
+        _state = State::READING_FILE_TYPE_ELEMENT;
+    } else if (name == "format-version") {
+        if (_foundFormatVersion) {
+            parseError("Only one 'format-version' element is allowed inside "
+                       "'meta'.");
+        }
+
+        _foundFormatVersion = true;
+        _state = State::READING_FORMAT_VERSION_ELEMENT;
+    } // We ignore other legal elements here for now.
+
+    _text.clear();              // we'll gather the element's contents
 }
 
 void DefaultTranslationParser::endElement(const char* name)
 {
-    if (_nestingLevel == 1) {
-        return;
+    const string expectedFileType = "FlightGear default translation file";
+    const string expectedFormatVersion = "1";
+
+    switch (_state) {
+    case State::LOOKING_FOR_RESOURCE_ELEMENT:
+        assert(false);
+        break;
+    case State::LOOKING_FOR_META_ELEMENT:
+        parseError("Expected a 'meta' element as the first child of 'resource'");
+    case State::READING_FILE_TYPE_ELEMENT:
+        _fileType = std::move(_text);
+
+        if (_fileType != expectedFileType) {
+            parseError("Expected body of 'file-type' element to be '" +
+                       expectedFileType + "', not '" + _fileType + "'");
+        }
+
+        _state = State::READING_META_ELEMENT;
+        break;
+    case State::READING_FORMAT_VERSION_ELEMENT:
+        _formatVersion = std::move(_text);
+
+        if (_formatVersion != expectedFormatVersion) {
+            parseError("Expected body of 'format-version' element to be '" +
+                       expectedFormatVersion + "', not '" + _formatVersion +
+                       "'");
+        }
+
+        _state = State::READING_META_ELEMENT;
+        break;
+    case State::READING_META_ELEMENT:
+        if (!std::strcmp(name, "meta")) {
+            checkIfFormatIsSupported(); // 'meta' element now finished, go!
+            _state = State::LOOKING_FOR_STRINGS_ELEMENT;
+        } // else it should be an end tag for other supported <meta> children,
+          // namely 'description' or 'language-description' for now.
+        break;
+    case State::LOOKING_FOR_STRINGS_ELEMENT:
+        // No <strings> element: the file has no translatable strings, no big
+        // deal
+        break;
+    case State::READING_TRANSLATABLE_STRING:
+        _resource->addTranslationUnit(std::move(_stringTagName),
+                                      _nextIndex[_stringTagName]++,
+                                      std::move(_text), _hasPlural);
+        _state = State::READING_STRINGS_ELEMENT;
+        break;
+    case State::READING_STRINGS_ELEMENT:
+        _state = State::AFTER_STRINGS_ELEMENT;
+        break;
+    case State::AFTER_STRINGS_ELEMENT:
+        assert(!std::strcmp(name, "resource"));
     }
-
-    assert (_nestingLevel == 2);
-    assert(name == _name);
-
-    _resource->addTranslationUnit(std::move(_name), _nextIndex[_name]++,
-                                  std::move(_sourceText), _hasPlural);
-    _nestingLevel--;
 }
 
 void DefaultTranslationParser::data(const char * s, int len)
 {
-    _sourceText += string(s, len);
+    _text += string(s, len);
 }
 
 void DefaultTranslationParser::warning(const char* message, int line,
@@ -81,6 +203,22 @@ void DefaultTranslationParser::warning(const char* message, int line,
 {
     SG_LOG(SG_GENERAL, SG_WARN, "Warning: " << message << " (line " << line
            << ", column " << column << ')');
+}
+
+void DefaultTranslationParser::checkIfFormatIsSupported()
+{
+    if (!_foundFileType) {
+        parseError("'file-type' element is required inside the 'meta' element");
+    } else if (!_foundFormatVersion) {
+        parseError("'format-version' element is required inside the 'meta' "
+                   "element");
+    }
+
+    // We've already checked the values of _fileType and _formatVersion as
+    // soon as we found them (otherwise, in case of a problem, the line and
+    // column info would be inaccurate). So, we're all good!
+
+    return;
 }
 
 } // namespace flightgear
--- src/Translations/DefaultTranslationParser.hxx
+++ src/Translations/DefaultTranslationParser.hxx
@@ -28,16 +28,40 @@ protected:
 
 private:
     bool asBoolean(const std::string& str);
+    [[noreturn]] void parseError(const std::string& message);
+    void startElementInsideMeta(const std::string& name);
+    // Check if we found supported 'file-type' and 'format-version' values
+    void checkIfFormatIsSupported();
 
     TranslationResource* _resource; // points to container for the transl. units
-    std::string _name;
-    std::string _sourceText;
-    bool _hasPlural = false;
-    // Number of elements found with a given name, at any time. Values in this
-    // map indicate the index to assign to the next element whose name is the
-    // key.
+    std::string _text;              // contents-so-far of the current XML element
+    std::string _stringTagName;     // tag name of a translatable string
+    bool _hasPlural = false;        // whether the string has plural forms
+    // Number of translatable strings found with a given _stringTagName, at
+    // any time. Values in this map indicate the index to assign to the next
+    // element (translatable string) whose tag name is the key.
     std::map<std::string, int> _nextIndex;
-    int _nestingLevel = 0;
+
+    // Our parser is a state machine
+    enum class State {
+        LOOKING_FOR_RESOURCE_ELEMENT, // this is the root element
+        LOOKING_FOR_META_ELEMENT,
+        READING_META_ELEMENT,
+        READING_FILE_TYPE_ELEMENT,
+        READING_FORMAT_VERSION_ELEMENT,
+        LOOKING_FOR_STRINGS_ELEMENT,
+        READING_STRINGS_ELEMENT,
+        READING_TRANSLATABLE_STRING,
+        AFTER_STRINGS_ELEMENT,
+    };
+
+    State _state = State::LOOKING_FOR_RESOURCE_ELEMENT;
+
+    // Information on the file format, read from its <meta> element
+    std::string _fileType;
+    bool _foundFileType = false;
+    std::string _formatVersion;
+    bool _foundFormatVersion = false;
 };
 
 } // namespace flightgear
