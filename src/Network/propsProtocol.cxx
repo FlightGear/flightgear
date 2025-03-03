6134ae706918d5d67d3e3e6ea15d2c9b0c982454
Mariusz Matuszek <MariuszXC@protected>
2025-03-03 15:44:41 +0100

    add 'about' menu item to telnet interface
    This provides an alternative way to obtain the same diagnostic
    information as given by gui/about dialog.
    
    Useful in cases, where the graphics subsystem did not initialize properly.

--- src/Network/propsProtocol.cxx
+++ src/Network/propsProtocol.cxx
@@ -37,6 +37,8 @@
 #include <string>
 #include <vector>
 
+#include "../Main/fg_props.hxx"
+
 
 using std::ends;
 using std::stringstream;
@@ -360,6 +362,26 @@ void FGProps::PropsChannel::foundTerminator()
                         push(text.str().c_str());
                     }
                 }
+            } else if (command == "about") {
+                stringstream aboutinfo;
+                aboutinfo << endl << "==== BEGIN ABOUT FG - CUT HERE ====" << endl;
+                aboutinfo << "/sim/version/flightgear: "      << fgGetString("/sim/version/flightgear") << endl;
+                aboutinfo << "/sim/version/simgear: "         << fgGetString("/sim/version/simgear") << endl;
+                aboutinfo << "/sim/version/openscenegraph: "  << fgGetString("/sim/version/openscenegraph") << endl;
+                aboutinfo << "/sim/version/build-id: "        << fgGetString("/sim/version/build-id") << endl;
+                aboutinfo << "/sim/version/build-number: "    << fgGetString("/sim/version/build-number") << endl;
+                aboutinfo << "/sim/version/build-type: "      << fgGetString("/sim/version/build-type") << endl;
+                aboutinfo << "/sim/version/revision: "        << fgGetString("/sim/version/revision") << endl;
+                aboutinfo << "/sim/rendering/gl-vendor: "       << fgGetString("/sim/rendering/gl-vendor") << endl;
+                aboutinfo << "/sim/rendering/gl-renderer: "     << fgGetString("/sim/rendering/gl-renderer") << endl;
+                aboutinfo << "/sim/rendering/gl-version: "      << fgGetString("/sim/rendering/gl-version") << endl;
+                aboutinfo << "/sim/rendering/gl-shading-language-version: " << fgGetString("/sim/rendering/gl-shading-language-version") << endl;
+                aboutinfo << "/sim/rendering/max-texture-size: "   << fgGetString("/sim/rendering/max-texture-size") << endl;
+                aboutinfo << "/sim/rendering/depth-buffer-bits: "  << fgGetString("/sim/rendering/depth-buffer-bits") << endl;
+                aboutinfo << "==== END ABOUT FG - CUT HERE ====" << endl << endl;
+                aboutinfo << ends;
+                push(aboutinfo.str().c_str());
+                push(getTerminator());
             } else if (command == "dump") {
                 stringstream buf;
                 if (tokens.size() <= 1) {
@@ -646,6 +668,7 @@ void FGProps::PropsChannel::foundTerminator()
                 const char* msg = "\
 Valid commands are:\r\n\
 \r\n\
+about              print 'about FG' data, useful for debugging\r\n\
 cd <dir>           cd to a directory, '..' to move back\r\n\
 data               switch to raw data mode\r\n\
 dump               dump current state (in xml)\r\n\
